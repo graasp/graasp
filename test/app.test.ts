f5d9c1af47c8c24e184e8aaa8d2531b1bf6acb50
André Nogueira <andre.nogueira@epfl.ch>
2021-07-23 21:06:37 +0200

    fix: await for plugins registration

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,27 +1,31 @@
-import fastifyServerInstance from '../src/app';
+import fastify from 'fastify';
+import registerAppPlugins from '../src/app';
+
+const fastifyInstance = fastify({});
+registerAppPlugins(fastifyInstance);
 
 describe('Items API', () => {
   afterAll(() => {
     // At the end of your tests it is highly recommended to call `.close()`
     // to ensure that all connections to external services get closed.
-    fastifyServerInstance.close();
+    fastifyInstance.close();
   });
 
   test('GET /itemssss should return 404', async () => {
     const { statusCode } =
-      await fastifyServerInstance.inject({ method: 'GET', url: '/itemssss' });
+      await fastifyInstance.inject({ method: 'GET', url: '/itemssss' });
     expect(statusCode).toBe(404);
   });
 
   test('GET /items w/o multiple &id=<> parameters should return 400', async () => {
     const { statusCode } =
-      await fastifyServerInstance.inject({ method: 'GET', url: '/items' });
+      await fastifyInstance.inject({ method: 'GET', url: '/items' });
     expect(statusCode).toBe(400);
   });
 
   test('GET /items/<fake-id> w/o user should return 401', async () => {
     const { statusCode } =
-      await fastifyServerInstance.inject({
+      await fastifyInstance.inject({
         method: 'GET',
         url: '/items/bec986c2-37f5-4cfd-b168-5a23a47b69a3',
         headers: { }
