1991a1d62ae8a7e6dee011d28fb339e4bbcc5b11
Julien Torrent <Julien.torrent@outlook.com>
2021-09-07 15:40:59 +0200

    fix: get own items calls postHookHandler

--- src/services/items/tasks/get-own-items-task.ts
+++ src/services/items/tasks/get-own-items-task.ts
@@ -1,4 +1,5 @@
 // global
+import { FastifyLoggerInstance } from 'fastify';
 import { DatabaseTransactionHandler } from '../../../plugins/database';
 // other services
 import { ItemMembershipService } from '../../../services/item-memberships/db-service';
@@ -16,13 +17,15 @@ export class GetOwnItemsTask extends BaseItemTask<Item[]> {
     super(member, itemService, itemMembershipService);
   }
 
-  async run(handler: DatabaseTransactionHandler): Promise<void> {
+  async run(handler: DatabaseTransactionHandler, log: FastifyLoggerInstance): Promise<void> {
     this.status = 'RUNNING';
 
     const memberId = this.actor.id;
 
     // get member's "own" items (created by member and where member is admin)
     const items = await this.itemService.getOwn(memberId, handler);
+
+    this.postHookHandler?.(items, this.actor, { log, handler });
 
     this.status = 'OK';
     this._result = items;
