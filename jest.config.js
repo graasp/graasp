0370c625c262eda92570d3b8d4514756bdf5abfc
pyphilia <pyphilia@gmail.com>
2022-08-03 10:35:06 +0200

    refactor: update dependencies

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,6 @@ module.exports = {
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest'
   },
-  transformIgnorePatterns: ['node_modules/(?!(crypto-random-string)/)'],
   testMatch: ['**/*.test.(ts|js)'],
   testEnvironment: 'node',
   // added for jest to look for the dependencies
