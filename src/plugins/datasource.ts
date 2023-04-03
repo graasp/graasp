fedf9f9785e86fe0752de4e43826c6ecf0c96c58
pyphilia <pyphilia@gmail.com>
2023-04-03 18:12:01 +0200

    refactor: enable logging, fix test workflow

--- src/plugins/datasource.ts
+++ src/plugins/datasource.ts
@@ -57,7 +57,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME, 
-  logging: ['error'],
+  logging: true,
   migrationsRun: true,
 
   // TODO: REMOVE
