5d56577c9a895abbeeca772634786685eff5b373
pyphilia <pyphilia@gmail.com>
2023-05-17 10:25:17 +0200

    refactor: add mailer test

---
+++ src/plugins/mailer/test/index.test.ts
@@ -0,0 +1,46 @@
+import { SUCCESS_MESSAGES } from '@graasp/translations';
+import build, { clearDatabase } from '../../../../test/app';
+
+// mock datasource
+jest.mock('../../../plugins/datasource');
+
+describe('Mailer', ()=>{
+  let app;
+  let actor;
+
+  beforeAll(async () => {
+    ({ app, actor } = await build());
+  });
+
+  afterAll(async () => {
+    jest.clearAllMocks();
+    await clearDatabase(app.db);
+    actor = null;
+    app.close();
+  });
+
+  describe('buildButton',()=>{
+    it('generates button given link and text', ()=>{
+      const link = 'mylink';
+      const text = 'mytext';
+      const button = app.mailer.buildButton(link, text);
+      expect(button).toContain(link);
+      expect(button).toContain(text);
+    });
+  });
+
+  describe('buildText',()=>{
+    it('generates text paragraph', ()=>{
+      const text = 'mytext';
+      const result = app.mailer.buildText( text);
+      expect(result).toContain(text);
+    });
+  });
+
+  describe('translate',()=>{
+    it('generates button given link and text', ()=>{
+      const translate = app.mailer.translate('fr');
+      expect(translate(SUCCESS_MESSAGES.COPY_ITEM).length).toBeGreaterThan(1);
+    });
+  });
+});
