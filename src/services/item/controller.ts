8d59398d09f73a518055e053d2aac2f4de1d7255
Morgan Ridel <ridelmorgan@gmail.com>
2023-06-12 13:23:51 +0000

    fix: h5p test more consistent

--- src/services/item/controller.ts
+++ src/services/item/controller.ts
@@ -140,11 +140,11 @@ const plugin: FastifyPluginAsync = async (fastify) => {
         body,
         log,
       } = request;
-      const item = await db.transaction((manager) => {
-        return itemService.patch(member, buildRepositories(manager), id, body);
+      return await db.transaction(async (manager) => {
+        const item = await itemService.patch(member, buildRepositories(manager), id, body);
+        await actionItemService.postPatchAction(request, reply, item);
+        return item;
       });
-      actionItemService.postPatchAction(request, reply, item);
-      return item;
     },
   );
 
@@ -161,18 +161,14 @@ const plugin: FastifyPluginAsync = async (fastify) => {
         body,
         log,
       } = request;
-      db.transaction((manager) => {
+      db.transaction(async (manager) => {
         // TODO: implement queue
-        return itemService.patchMany(member, buildRepositories(manager), ids, body);
-      })
-        .then((resultItems) => {
-          // do not wait
-          actionItemService.postManyPatchAction(request, reply, resultOfToList(resultItems));
-        })
-        .catch((e) => {
-          // TODO: return feedback in queue
-          console.error(e);
-        });
+        const items = await itemService.patchMany(member, buildRepositories(manager), ids, body);
+        await actionItemService.postManyPatchAction(request, reply, resultOfToList(items));
+      }).catch((e) => {
+        // TODO: return feedback in queue
+        console.error(e);
+      });
       reply.status(StatusCodes.ACCEPTED);
       return ids;
     },
@@ -208,18 +204,14 @@ const plugin: FastifyPluginAsync = async (fastify) => {
         query: { id: ids },
         log,
       } = request;
-      db.transaction((manager) => {
+      db.transaction(async (manager) => {
         // TODO: implement queue
-        return itemService.deleteMany(member, buildRepositories(manager), ids);
-      })
-        .then((items) => {
-          // do not wait
-          actionItemService.postManyDeleteAction(request, reply, items);
-        })
-        .catch((e) => {
-          // TODO: return feedback in queue
-          console.error(e);
-        });
+        const items = await itemService.deleteMany(member, buildRepositories(manager), ids);
+        await actionItemService.postManyDeleteAction(request, reply, items);
+      }).catch((e) => {
+        // TODO: return feedback in queue
+        console.error(e);
+      });
       reply.status(StatusCodes.ACCEPTED);
       return ids;
     },
@@ -255,16 +247,12 @@ const plugin: FastifyPluginAsync = async (fastify) => {
       } = request;
       // TODO: implement queue
       db.transaction(async (manager) => {
-        return itemService.moveMany(member, buildRepositories(manager), ids, parentId);
-      })
-        .then((items) => {
-          // we do not wait
-          actionItemService.postManyMoveAction(request, reply, items);
-        })
-        .catch((e) => {
-          // TODO: return feedback in queue
-          console.error(e);
-        });
+        const items = await itemService.moveMany(member, buildRepositories(manager), ids, parentId);
+        await actionItemService.postManyMoveAction(request, reply, items);
+      }).catch((e) => {
+        // TODO: return feedback in queue
+        console.error(e);
+      });
       reply.status(StatusCodes.ACCEPTED);
       return ids;
     },
@@ -305,18 +293,15 @@ const plugin: FastifyPluginAsync = async (fastify) => {
       } = request;
       // TODO: implement queue
       db.transaction(async (manager) => {
-        return itemService.copyMany(member, buildRepositories(manager), ids, {
+        // await new Promise(r => setTimeout(r, 5000));
+        const items = await itemService.copyMany(member, buildRepositories(manager), ids, {
           parentId,
         });
-      })
-        .then((items) => {
-          // do not wait
-          actionItemService.postManyCopyAction(request, reply, items);
-        })
-        .catch((e) => {
-          // TODO: return feedback in queue
-          console.error(e);
-        });
+        await actionItemService.postManyCopyAction(request, reply, items);
+      }).catch((e) => {
+        // TODO: return feedback in queue
+        console.error(e);
+      });
       reply.status(StatusCodes.ACCEPTED);
       return ids;
     },
