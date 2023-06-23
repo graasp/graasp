345c20167e6e0431691b3f8dd491176d78aa1273
pyphilia <pyphilia@gmail.com>
2023-06-23 13:15:54 +0000

    feat: allow to edit file item extra's alttext

--- src/services/item/plugins/file/test/index.test.ts
+++ src/services/item/plugins/file/test/index.test.ts
@@ -442,6 +442,35 @@ describe('File Item routes tests', () => {
     });
   });
 
+  describe('Edit file - PATCH /items/id', () => {
+    let item, member;
+
+    beforeEach(async () => {
+      ({ app } = await build());
+      member = await saveMember(BOB);
+      ({ item } = await saveItemAndMembership({ item: MOCK_FILE_ITEM, member }));
+    });
+
+    it('Edit file item altText', async () => {
+      const response = await app.inject({
+        method: HttpMethod.PATCH,
+        url: `${ITEMS_ROUTE_PREFIX}/${item.id}`,
+        payload: { extra: { [FILE_ITEM_TYPE]: { altText: 'new name' } } },
+      });
+      console.log(response.json());
+      expect(response.json()).toMatchObject(new MemberCannotAccess(expect.anything()));
+    });
+
+    it('Cannot edit another file item field', async () => {
+      const response = await app.inject({
+        method: HttpMethod.PATCH,
+        url: `${ITEMS_ROUTE_PREFIX}/${item.id}`,
+        payload: { extra: { [FILE_ITEM_TYPE]: { size: 10 } } },
+      });
+      expect(response.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+    });
+  });
+
   describe('Hooks', () => {
     beforeEach(async () => {
       ({ app, actor } = await build());
