59c8d014f9b15656a42422ad54e217c310a50e5c
kim <kim.phanhoang@epfl.ch>
2023-07-20 09:59:10 +0200

    refactor: fix missing semi colon

--- src/migrations/1689777747530-default-item-settings.ts
+++ src/migrations/1689777747530-default-item-settings.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class Migrations1689777747530 implements MigrationInterface {
-    name = 'Migrations1689777747530'
+    name = 'Migrations1689777747530';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "item" ALTER COLUMN "settings" DROP DEFAULT`);
