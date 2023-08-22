023775fcaec33d15a6c048186d74e3189a0339a7
kim <kim.phanhoang@epfl.ch>
2023-08-22 10:51:53 +0200

    refactor: add informatics, physical-education

--- src/migrations/1692624998160-category-seed.ts
+++ src/migrations/1692624998160-category-seed.ts
@@ -35,6 +35,17 @@ export class Migrations1692624998160 implements MigrationInterface {
     await update('Natural Science', 'natural-science');
     await update('Social Science', 'social-science');
 
+    await queryRunner.query(`INSERT INTO category (name, type) VALUES (
+      'informatics',
+      'discipline'
+      );
+    `);
+    await queryRunner.query(`INSERT INTO category (name, type) VALUES (
+        'physical-education',
+        'discipline'
+        );
+    `);
+
     // update categories for level
     await update('Kindergarden', 'kindergarten');
     await update('Primary School', 'primary-school');
@@ -62,13 +73,19 @@ export class Migrations1692624998160 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    // lang
     await queryRunner.query(`DELETE FROM category WHERE name = 'arabic'`);
     await queryRunner.query(`DELETE FROM category WHERE name = 'italian'`);
+    await queryRunner.query(`UPDATE FROM category SET name='English' WHERE name = 'english'`);
+    await queryRunner.query(`UPDATE FROM category SET name='French' WHERE name = 'french'`);
+    await queryRunner.query(`UPDATE FROM category SET name='German' WHERE name = 'german'`);
 
+    // resource-type
     await queryRunner.query(`DELETE FROM category WHERE name = 'template'`);
     await queryRunner.query(`DELETE FROM category WHERE name = 'collection'`);
     await queryRunner.query(`DELETE FROM category WHERE name = 'app'`);
 
+    // discipline
     await queryRunner.query(`UPDATE FROM category SET name='Arts' WHERE name = 'arts'`);
     await queryRunner.query(`UPDATE FROM category SET name='Language' WHERE name = 'language'`);
     await queryRunner.query(`UPDATE FROM category SET name='Math' WHERE name = 'mathematics'`);
@@ -79,9 +96,14 @@ export class Migrations1692624998160 implements MigrationInterface {
     await queryRunner.query(
       `UPDATE FROM category SET name='Social Science' WHERE name = 'social-science'`,
     );
+    await queryRunner.query(`DELETE FROM category WHERE name = 'physical-education'`);
+    await queryRunner.query(`DELETE FROM category WHERE name = 'informatics'`);
 
+    await queryRunner.query(`DELETE FROM category WHERE name = 'template'`);
+
+    // level
     await queryRunner.query(
-      `UPDATE FROM category SET name='Kindergarten' WHERE name = 'kindergarten'`,
+      `UPDATE FROM category SET name='Kindergarden' WHERE name = 'kindergarten'`,
     );
     await queryRunner.query(
       `UPDATE FROM category SET name='Primary School' WHERE name = 'primary-school'`,
