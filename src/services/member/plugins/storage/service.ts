6ad68cdb4d9ce73cc5c4a81999df5aa76af0cc0b
kim <kim.phanhoang@epfl.ch>
2023-08-23 16:59:01 +0200

    feat: implement get current member storage

---
+++ src/services/member/plugins/storage/service.ts
@@ -0,0 +1,43 @@
+import { FileItemType } from '@graasp/sdk';
+
+import { Repositories } from '../../../../utils/repositories';
+import { DEFAULT_MAX_STORAGE } from '../../../item/plugins/file/utils/constants';
+import { StorageExceeded } from '../../../item/plugins/file/utils/errors';
+import { Member } from '../../entities/member';
+
+export class StorageService {
+  fileItemType: FileItemType;
+
+  constructor(fileItemType: FileItemType) {
+    this.fileItemType = fileItemType;
+  }
+
+  async getMaximumStorageSize(_actor: Member): Promise<number> {
+    // todo: depends on user/group
+    return DEFAULT_MAX_STORAGE;
+  }
+
+  async getStorageLimits(actor: Member, type: FileItemType, { itemRepository }: Repositories) {
+    return {
+      current: await itemRepository.getItemSumSize(actor?.id, type),
+      maximum: await this.getMaximumStorageSize(actor),
+    };
+  }
+
+  // check the user has enough storage to create a new item given its size
+  // get the complete storage
+  // todo: include more item types
+  async checkRemainingStorage(actor: Member, repositories: Repositories, size: number = 0) {
+    const { id: memberId } = actor;
+
+    const currentStorage = await repositories.itemRepository.getItemSumSize(
+      memberId,
+      this.fileItemType,
+    );
+
+    const maxStorage = await this.getMaximumStorageSize(actor);
+    if (currentStorage + size > maxStorage) {
+      throw new StorageExceeded(currentStorage + size);
+    }
+  }
+}
