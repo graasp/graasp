e1b7b7ea58173bda1f93bfde0401fbfd86e86bef
Alexandre Chau <accounts@chau.moe>
2023-09-26 08:59:19 +0000

    fix: mock s3 methods in action ws tests

--- src/services/item/plugins/action/test/ws.test.ts
+++ src/services/item/plugins/action/test/ws.test.ts
@@ -13,6 +13,38 @@ import { ActionRequestExportRepository } from '../requestExport/repository';
 // mock datasource
 jest.mock('../../../../../plugins/datasource');
 
+const uploadDoneMock = jest.fn(async () => console.debug('aws s3 storage upload'));
+const deleteObjectMock = jest.fn(async () => console.debug('deleteObjectMock'));
+const headObjectMock = jest.fn(async () => console.debug('headObjectMock'));
+const MOCK_SIGNED_URL = 'signed-url';
+jest.mock('@aws-sdk/client-s3', () => {
+  return {
+    GetObjectCommand: jest.fn(),
+    S3: function () {
+      return {
+        deleteObject: deleteObjectMock,
+        putObject: uploadDoneMock,
+        headObject: headObjectMock,
+      };
+    },
+  };
+});
+jest.mock('@aws-sdk/s3-request-presigner', () => {
+  const getSignedUrl = jest.fn(async () => MOCK_SIGNED_URL);
+  return {
+    getSignedUrl,
+  };
+});
+jest.mock('@aws-sdk/lib-storage', () => {
+  return {
+    Upload: jest.fn().mockImplementation(() => {
+      return {
+        done: uploadDoneMock,
+      };
+    }),
+  };
+});
+
 describe('asynchronous feedback', () => {
   let app, actor, address;
   let ws: TestWsClient;
