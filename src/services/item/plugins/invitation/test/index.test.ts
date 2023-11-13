60374a6ff8c63acbe14eab6ba98ebfe7d6712e33
marcelmmc <marcel.morancalderon@epfl.ch>
2023-11-13 17:09:17 +0100

    chore: add endpoint to handle invitations
    chore: remove commented code and add correct yarn.lock
    
    chore: allow selection of template and amend comments in PR
    
    chore: ammend comments, clean code and reduce logic
    
    chore: avoid casting item when setting item for invitation
    
    chore: avoid group service on non-folder items
    
    fix: import issues
    
    fix: changes
    
    chore: add unit test for upload-csv end-point
    
    chore: save changes for tests
    
    chore: adding test for grouping
    
    chore: add tests for non creation group csv
    
    chore: reformat code
    
    chore: folder is not being captured
    
    fix: small changes before rebase

--- src/services/item/plugins/invitation/test/index.test.ts
+++ src/services/item/plugins/invitation/test/index.test.ts
@@ -1,21 +1,42 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
+import FormData from 'form-data';
+import fs from 'fs';
 import { StatusCodes } from 'http-status-codes';
 import fetch from 'node-fetch';
+import * as Papa from 'papaparse';
+import path from 'path';
 import { In } from 'typeorm';
+import waitForExpect from 'wait-for-expect';
 
-import { HttpMethod, PermissionLevel, RecaptchaAction } from '@graasp/sdk';
+import { MultipartFile } from '@fastify/multipart';
+import fastify, { FastifyRequest } from 'fastify';
+
+import {
+  CompleteMember,
+  DiscriminatedItem,
+  FolderItemFactory,
+  HttpMethod,
+  ItemMembership,
+  ItemType,
+  MemberFactory,
+  PermissionLevel,
+  RecaptchaAction,
+} from '@graasp/sdk';
 
 import build, { clearDatabase } from '../../../../../../test/app';
 import { ITEMS_ROUTE_PREFIX } from '../../../../../utils/config';
 import { MOCK_CAPTCHA } from '../../../../auth/plugins/captcha/test/utils';
-import { Item } from '../../../../item/entities/Item';
 import { generateRandomEmail } from '../../../../itemLogin/utils';
 import { ItemMembershipRepository } from '../../../../itemMembership/repository';
 import { Member } from '../../../../member/entities/member';
-import { saveMember } from '../../../../member/test/fixtures/members';
+import { saveMember, saveMembers } from '../../../../member/test/fixtures/members';
+import { FolderItem, Item } from '../../../entities/Item';
+import { ItemRepository } from '../../../repository';
 import { ItemTestUtils } from '../../../test/fixtures/items';
 import { Invitation } from '../entity';
 import { InvitationRepository } from '../repository';
+import { CSVInvite, parseCSV } from '../utils';
+import { MissingEmailColumnInCSVError, MissingEmailInRowError } from '../errors';
 
 // mock datasource
 jest.mock('../../../../../plugins/datasource');
@@ -539,5 +560,369 @@ describe('Invitation Plugin', () => {
         }, 1000);
       });
     });
+  });
+});
+
+describe('Group endpoint', () => {
+  let app;
+  let actor;
+
+  beforeEach(async () => {
+    ({ app, actor } = await build());
+  });
+
+  afterEach(async () => {
+    jest.clearAllMocks();
+    await clearDatabase(app.db);
+    actor = null;
+    app.close();
+  });
+
+  const createForm = async (filePath: string) => {
+    const file = fs.createReadStream(filePath);
+    const form = new FormData();
+    form.append('myfile', file);
+    return form;
+  };
+
+  const creatFilePath = async (filename: string) => {
+    return path.resolve(__dirname, `./fixtures/${filename}`);
+  };
+
+  const readCSVInvitations = async (filePath: string) => {
+    const csvFile = fs.createReadStream(filePath);
+    const { rows, header } = await parseCSV(csvFile);
+    return rows;
+  };
+
+  const createExpectedItemMemberships = async (rows: CSVInvite[], item?: Item) => {
+    const memRows = rows.reduce<{
+      [key: string]: {
+        member: Pick<Member, 'email'>;
+        item: Pick<Item, 'name' | 'type'>;
+        permission: string;
+      };
+    }>((resMap, row) => {
+      resMap[row.email] = {
+        member: { email: row.email },
+        item: { name: item ? item.name : row.group_name ?? '', type: ItemType.FOLDER },
+        permission: row.permission ?? PermissionLevel.Read,
+      };
+      return resMap;
+    }, {});
+    return memRows;
+  };
+
+  const createExpectedInvitations = async (rows: CSVInvite[], item?: Item) => {
+    const invRows = rows.reduce<{
+      [key: string]: {
+        email: string;
+        item: Pick<Item, 'name' | 'type'>;
+        permission: string;
+      };
+    }>((resMap, row) => {
+      resMap[row.email] = {
+        email: row.email,
+        item: { name: item ? item.name : row.group_name ?? '', type: ItemType.FOLDER },
+        permission: row.permission ?? PermissionLevel.Read,
+      };
+      return resMap;
+    }, {});
+    return invRows;
+  };
+
+  const compareResponse = async (
+    data: (ItemMembership | Invitation)[],
+    expMem?: {
+      [key: string]: {
+        member: Pick<Member, 'email'>;
+        item: Pick<Item, 'name' | 'type'>;
+        permission: string;
+      };
+    },
+    expInv?: {
+      [key: string]: {
+        email: string;
+        item: Pick<Item, 'name' | 'type'>;
+        permission: string;
+      };
+    },
+  ) => {
+    const expectedSize =
+      (expMem ? Object.keys(expMem).length : 0) + (expInv ? Object.keys(expInv).length : 0);
+
+    expect(data).toHaveLength(expectedSize);
+    data.map(async (e) => {
+      let entryToCompare;
+      if (e.hasOwnProperty('member')) {
+        entryToCompare = expMem ? expMem[(e as ItemMembership).member.email] : undefined;
+      } else {
+        entryToCompare = expInv ? expInv[(e as Invitation).email] : undefined;
+      }
+      expect(e).toMatchObject(entryToCompare);
+    });
+  };
+
+  const uploadInjection = async (form: FormData, item: Item, idTemplate: string = '') => {
+    const response = await app.inject({
+      method: HttpMethod.Post,
+      url: `${ITEMS_ROUTE_PREFIX}/${item.id}/invitations/upload-csv?id=${item.id}&template_id=${idTemplate}`,
+      payload: form,
+      headers: form.getHeaders(),
+    });
+    return response;
+  };
+  // ${API_HOST}/items/${_itemId}/invitations/upload_csv?id=${_itemId}&template_id=${idTemplate}
+  it('Upload a group CSV all members', async () => {
+    //By default save Item will create an item
+    const { item } = await testUtils.saveItemAndMembership({
+      member: actor,
+    });
+    const mockSendMail = mockEmail(app);
+    const filePath = await creatFilePath('group.csv');
+    const form = await createForm(filePath);
+    const rowsInvitations = await readCSVInvitations(filePath);
+    const expItemMem = await createExpectedItemMemberships(rowsInvitations);
+    const protoMembers: CompleteMember[] = rowsInvitations.map((row) =>
+      MemberFactory({ email: row.email }),
+    );
+    await saveMembers(protoMembers);
+    const response = await uploadInjection(form, item);
+    expect(response.statusCode).toBe(StatusCodes.OK);
+    const data: ItemMembership[] = JSON.parse(response.body).data;
+    compareResponse(data, expItemMem);
+    await waitForExpect(() => {
+      expect(mockSendMail).toHaveBeenCalledTimes(rowsInvitations.length);
+    });
+  });
+
+  it('Upload a group CSV all invitations', async () => {
+    //By default save Item will create an item
+    const { item } = await testUtils.saveItemAndMembership({
+      member: actor,
+    });
+    const mockSendMail = mockEmail(app);
+    const filePath = await creatFilePath('group.csv');
+    const form = await createForm(filePath);
+    const rowsInvitations = await readCSVInvitations(filePath);
+    const expInv = await createExpectedInvitations(rowsInvitations);
+    const response = await uploadInjection(form, item);
+    expect(response.statusCode).toBe(StatusCodes.OK);
+    const data: ItemMembership[] = JSON.parse(response.body).data;
+    compareResponse(data, undefined, expInv);
+    await waitForExpect(() => {
+      expect(mockSendMail).toHaveBeenCalledTimes(rowsInvitations.length);
+    });
+  });
+
+  it('Upload a group CSV mix members and invitations', async () => {
+    //By default save Item will create an item
+    const { item } = await testUtils.saveItemAndMembership({
+      member: actor,
+    });
+    const mockSendMail = mockEmail(app);
+    const filePath = await creatFilePath('group.csv');
+    const form = await createForm(filePath);
+    const rowsInvitations = await readCSVInvitations(filePath);
+
+    const firstHalfOfRows = rowsInvitations.slice(0, rowsInvitations.length / 2);
+    const secondHalfOfRows = rowsInvitations.slice(
+      rowsInvitations.length / 2,
+      rowsInvitations.length,
+    );
+    const protoMembers: CompleteMember[] = firstHalfOfRows.map((row) =>
+      MemberFactory({ email: row.email }),
+    );
+    const members = await saveMembers(protoMembers);
+    const expItemMem = await createExpectedItemMemberships(firstHalfOfRows);
+    const expInv = await createExpectedInvitations(secondHalfOfRows);
+    const response = await uploadInjection(form, item);
+
+    expect(response.statusCode).toBe(StatusCodes.OK);
+
+    const data: (ItemMembership | Invitation)[] = JSON.parse(response.body).data;
+
+    compareResponse(data, expItemMem, expInv);
+    await waitForExpect(() => {
+      expect(mockSendMail).toHaveBeenCalledTimes(rowsInvitations.length);
+    });
+  });
+
+  it('Check error missing a column email CSV', async () => {
+    const { item } = await testUtils.saveItemAndMembership({
+      member: actor,
+    });
+    const mockSendMail = mockEmail(app);
+    const filePath = await creatFilePath('email_column_missing.csv');
+    const form = await createForm(filePath);
+    const response = await uploadInjection(form, item);
+    expect(response.json()).toEqual(new MissingEmailColumnInCSVError());
+    expect(response.statusCode).toBe(StatusCodes.BAD_REQUEST);
+    await waitForExpect(() => {
+      expect(mockSendMail).toHaveBeenCalledTimes(0);
+    });
+  });
+
+  it('Check error missing email entries CSV', async () => {
+    const { item } = await testUtils.saveItemAndMembership({
+      member: actor,
+    });
+    const mockSendMail = mockEmail(app);
+    const filePath = await creatFilePath('missing_email_entries.csv');
+    const form = await createForm(filePath);
+    const response = await uploadInjection(form, item);
+    const rowsInvitations = await readCSVInvitations(filePath);
+
+    const rowsWithoutEmail = rowsInvitations.filter((row) => !row.email);
+    expect(response.json()).toEqual(new MissingEmailInRowError(rowsWithoutEmail));
+    expect(response.statusCode).toBe(StatusCodes.BAD_REQUEST);
+    await waitForExpect(() => {
+      expect(mockSendMail).toHaveBeenCalledTimes(0);
+    });
+  });
+
+  it('Upload a normal CSV all memberships', async () => {
+    //By default save Item will create an item
+    const { item } = await testUtils.saveItemAndMembership({
+      member: actor,
+    });
+    const mockSendMail = mockEmail(app);
+    const filePath = await creatFilePath('sharing_current_item.csv');
+    const form = await createForm(filePath);
+    const rowsInvitations = await readCSVInvitations(filePath);
+    const expItemMem = await createExpectedItemMemberships(rowsInvitations, item);
+    const protoMembers: CompleteMember[] = rowsInvitations.map((row) =>
+      MemberFactory({ email: row.email }),
+    );
+    await saveMembers(protoMembers);
+    const response = await uploadInjection(form, item);
+    expect(response.statusCode).toBe(StatusCodes.OK);
+    const data: ItemMembership[] = JSON.parse(response.body).data;
+    compareResponse(data, expItemMem);
+    await waitForExpect(() => {
+      expect(mockSendMail).toHaveBeenCalledTimes(rowsInvitations.length);
+    });
+  });
+
+  it('Upload a normal CSV all invitation', async () => {
+    //By default save Item will create an item
+    const { item } = await testUtils.saveItemAndMembership({
+      member: actor,
+    });
+    const mockSendMail = mockEmail(app);
+    const filePath = await creatFilePath('sharing_current_item.csv');
+    const form = await createForm(filePath);
+    const rowsInvitations = await readCSVInvitations(filePath);
+    const expInv = await createExpectedInvitations(rowsInvitations);
+    const response = await uploadInjection(form, item);
+    expect(response.statusCode).toBe(StatusCodes.OK);
+    const data: ItemMembership[] = JSON.parse(response.body).data;
+    compareResponse(data, undefined, expInv);
+    await waitForExpect(() => {
+      expect(mockSendMail).toHaveBeenCalledTimes(rowsInvitations.length);
+    });
+  });
+
+  it('Upload a normal CSV mix invitations and memberships', async () => {
+    //By default save Item will create an item
+    const { item } = await testUtils.saveItemAndMembership({
+      member: actor,
+    });
+    const mockSendMail = mockEmail(app);
+    const filePath = await creatFilePath('sharing_current_item.csv');
+    const form = await createForm(filePath);
+    const rowsInvitations = await readCSVInvitations(filePath);
+
+    const firstHalfOfRows = rowsInvitations.slice(0, rowsInvitations.length / 2);
+    const secondHalfOfRows = rowsInvitations.slice(
+      rowsInvitations.length / 2,
+      rowsInvitations.length,
+    );
+    const protoMembers: CompleteMember[] = firstHalfOfRows.map((row) =>
+      MemberFactory({ email: row.email }),
+    );
+    const members = await saveMembers(protoMembers);
+    const expItemMem = await createExpectedItemMemberships(firstHalfOfRows, item);
+    const expInv = await createExpectedInvitations(secondHalfOfRows, item);
+    const response = await uploadInjection(form, item);
+
+    expect(response.statusCode).toBe(StatusCodes.OK);
+
+    const data: (ItemMembership | Invitation)[] = JSON.parse(response.body).data;
+
+    compareResponse(data, expItemMem, expInv);
+    await waitForExpect(() => {
+      expect(mockSendMail).toHaveBeenCalledTimes(rowsInvitations.length);
+    });
+  });
+  const buildFolderItem = (
+    args: {
+      parentItem?: FolderItem;
+      extra?: { folder: { childrenOrder: string[] } };
+    } = {},
+  ) => {
+    const item = FolderItemFactory(args) as unknown as FolderItem;
+    // change date time for it to work with the backend data
+    item.createdAt = new Date(item.createdAt);
+    return item;
+  };
+
+  // const createExpectedFolderStructure = async (groupFolderNames: string[]) => {};
+  it('Upload a group CSV mix members and invitations template folder', async () => {
+    //By default save Item will create an item
+    const { item } = await testUtils.saveItemAndMembership({
+      member: actor,
+    });
+    const { item: templateItem } = await testUtils.saveItemAndMembership({
+      member: actor,
+    });
+    const subFolder = await testUtils.saveItem({
+      item: { name: 'sub' },
+      actor: actor,
+      parentItem: templateItem,
+    });
+
+    //check the folder where the folders are create empty
+    let children = await testUtils.itemRepository.getChildren(item);
+    expect(children).toEqual([]);
+    const mockSendMail = mockEmail(app);
+    const filePath = await creatFilePath('group.csv');
+    const form = await createForm(filePath);
+    const rowsInvitations = await readCSVInvitations(filePath);
+    console.log(rowsInvitations);
+    const firstHalfOfRows = rowsInvitations.slice(0, rowsInvitations.length / 2);
+    console.log(firstHalfOfRows);
+    const secondHalfOfRows = rowsInvitations.slice(
+      rowsInvitations.length / 2,
+      rowsInvitations.length,
+    );
+    console.log(secondHalfOfRows);
+
+    const protoMembers: CompleteMember[] = firstHalfOfRows.map((row) =>
+      MemberFactory({ email: row.email }),
+    );
+    const members = await saveMembers(protoMembers);
+    const expItemMem = await createExpectedItemMemberships(firstHalfOfRows);
+    const expInv = await createExpectedInvitations(secondHalfOfRows);
+    const response = await uploadInjection(form, item, templateItem.id);
+
+    expect(response.statusCode).toBe(StatusCodes.OK);
+
+    const data: (ItemMembership | Invitation)[] = JSON.parse(response.body).data;
+
+    compareResponse(data, expItemMem, expInv);
+    await waitForExpect(() => {
+      expect(mockSendMail).toHaveBeenCalledTimes(rowsInvitations.length);
+    });
+    const allItems = await testUtils.rawItemRepository.find();
+    console.log('all', allItems);
+    children = await testUtils.itemRepository.getDescendants(templateItem as FolderItem);
+    console.log(children);
+    children = await testUtils.itemRepository.getDescendants(item as FolderItem);
+    children.map(async (child) => {
+      const desc = await testUtils.itemRepository.getDescendants(child as FolderItem);
+      console.log(desc);
+    });
+    console.log(children);
   });
 });
--- src/services/item/plugins/invitation/utils.test.ts
+++ src/services/item/plugins/invitation/utils.test.ts
@@ -1,7 +1,10 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
-import { parseCSV } from './utils';
+import { MultipartFile } from '@fastify/multipart';
+
+import { CSV_MIMETYPE } from './constants';
+import { parseCSV, verifyCSVFileFormat } from './utils';
 
 describe('Test utils', () => {
   describe('parseCSV', () => {
@@ -12,5 +15,21 @@ describe('Test utils', () => {
       const { rows } = await parseCSV(fileStream);
       expect(rows).toEqual([{ name: 'Alice', email: 'alice@graasp.org', permission: 'read' }]);
     });
+  });
+});
+
+describe('Verify CSV format', () => {
+  it('send correct CSV format', async () => {
+    const uploadPayload = { mimetype: CSV_MIMETYPE } as MultipartFile;
+    expect(() => {
+      verifyCSVFileFormat(uploadPayload);
+    }).not.toThrow();
+  });
+
+  it('send incorrect CSV format', async () => {
+    const uploadPayload = { mimetype: 'wrong-mimetype' } as MultipartFile;
+    expect(() => {
+      verifyCSVFileFormat(uploadPayload);
+    }).toThrow();
   });
 });
