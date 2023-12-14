e77fc80b0beebdaf46ab00f7720d8242d5e5666c
spaenleh <spaenleh@gmail.com>
2023-12-14 12:25:10 +0100

    fix: migrations

--- src/migrations/1702552816415-appsetting-indexes.ts
+++ src/migrations/1702552816415-appsetting-indexes.ts
@@ -1,15 +1,19 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class Migrations1702456572289 implements MigrationInterface {
-  name = 'Migrations1702456572289';
+export class Migrations1702552816415 implements MigrationInterface {
+  name = 'Migrations1702552816415';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `CREATE INDEX "IDX_61546c650608c1e68789c64915" ON "app_setting" ("item_id", "name") `,
     );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_6079b3bb63c13f815f7dd8d8a2" ON "app_data" ("type") `,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`DROP INDEX "public"."IDX_61546c650608c1e68789c64915"`);
+    await queryRunner.query(`DROP INDEX "public"."IDX_6079b3bb63c13f815f7dd8d8a2"`);
   }
 }
