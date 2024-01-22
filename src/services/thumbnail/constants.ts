8824aa80a5e91e68e3ba0e560b6815e52f292fe7
spaenleh <spaenleh@gmail.com>
2024-01-22 17:11:06 +0100

    fix: use WebP for thumbnails and adapt sizes

--- src/services/thumbnail/constants.ts
+++ src/services/thumbnail/constants.ts
@@ -1,13 +1,22 @@
 import { MimeTypes, ThumbnailSize } from '@graasp/sdk';
 
 export const ThumbnailSizeFormat = {
-  [ThumbnailSize.Small]: 200,
-  [ThumbnailSize.Medium]: 400,
-  [ThumbnailSize.Large]: 600,
+  /**
+   * For use in Avatars, thumbnails in table etc
+   */
+  [ThumbnailSize.Small]: 40,
+  /**
+   * For use in library cards, builder grid view, etc
+   */
+  [ThumbnailSize.Medium]: 256,
+  /**
+   * For use when the image is the main focus, in the summary of the library for example
+   */
+  [ThumbnailSize.Large]: 512,
   [ThumbnailSize.Original]: undefined,
 };
 
-export const THUMBNAIL_FORMAT = 'png';
-export const THUMBNAIL_MIMETYPE = MimeTypes.Image.PNG;
+export const THUMBNAIL_FORMAT = 'webp';
+export const THUMBNAIL_MIMETYPE = MimeTypes.Image.WEBP;
 
 export const PLUGIN_NAME = 'graasp-plugin-thumbnails';
