e7665c0fed5a9c96243ad154cc0483229bd1fcad
Thibault Reidy <thibault.reidy@hotmail.ch>
2024-04-17 14:30:14 +0000

    feat: set HTTP action to POST and reply with no content

--- src/services/member/plugins/data/controller.ts
+++ src/services/member/plugins/data/controller.ts
@@ -1,3 +1,5 @@
+import { StatusCodes } from 'http-status-codes';
+
 import { FastifyPluginAsync } from 'fastify';
 
 import { buildRepositories } from '../../../../utils/repositories';
@@ -7,24 +9,32 @@ const plugin: FastifyPluginAsync = async (fastify) => {
   const {
     files: { service: fileService },
     mailer,
+    db,
   } = fastify;
   const dataMemberService = new DataMemberService();
 
   // download all related data to the given user
-  fastify.get<{ Params: { memberId: string } }>(
+  fastify.post(
     '/',
     {
       preHandler: fastify.verifyAuthentication,
     },
-    async ({ member }) => {
-      const repositories = buildRepositories();
+    async ({ member }, reply) => {
+      db.transaction(async (manager) => {
+        const repositories = buildRepositories(manager);
 
-      return dataMemberService.requestDataExport({
-        actor: member,
-        repositories,
-        fileService,
-        mailer,
+        await dataMemberService.requestDataExport({
+          actor: member,
+          repositories,
+          fileService,
+          mailer,
+        });
+
+        // TODO: no need to web sockets ?
       });
+
+      // reply no content and let the server create the archive and send the mail
+      reply.status(StatusCodes.NO_CONTENT);
     },
   );
 };
