046d35cddc6f5989c07cc8fec6361e39b4b66ef6
Thibault Reidy <thibault.reidy@hotmail.ch>
2024-04-26 12:11:59 +0000

    feat: remove copy and move WS hooks

--- src/services/item/test/ws.test.ts
+++ src/services/item/test/ws.test.ts
@@ -1,25 +1,14 @@
 import { StatusCodes } from 'http-status-codes';
-import { Not } from 'typeorm';
 import waitForExpect from 'wait-for-expect';
 
-import { HttpMethod, Websocket, parseStringToDate } from '@graasp/sdk';
+import { HttpMethod } from '@graasp/sdk';
 
 import { clearDatabase } from '../../../../test/app';
-import { MemberCannotAccess } from '../../../utils/errors';
-import { saveMember } from '../../member/test/fixtures/members';
 import { TestWsClient } from '../../websockets/test/test-websocket-client';
 import { setupWsApp } from '../../websockets/test/ws-app';
-import { FolderItem, Item } from '../entities/Item';
+import { FolderItem } from '../entities/Item';
 import { ItemRepository } from '../repository';
-import {
-  AccessibleItemsEvent,
-  ChildItemEvent,
-  ItemEvent,
-  ItemOpFeedbackEvent,
-  OwnItemsEvent,
-  itemTopic,
-  memberItemsTopic,
-} from '../ws/events';
+import { ItemEvent, ItemOpFeedbackEvent, memberItemsTopic } from '../ws/events';
 import { ItemTestUtils, expectItem } from './fixtures/items';
 
 // mock datasource
@@ -41,248 +30,6 @@ describe('Item websocket hooks', () => {
     actor = null;
     app.close();
     ws.close();
-  });
-
-  describe('Subscribe to item', () => {
-    it('subscribes to own item successfully', async () => {
-      const { item } = await testUtils.saveItemAndMembership({ member: actor });
-      const request = {
-        realm: Websocket.Realms.Notif,
-        topic: itemTopic,
-        channel: item.id,
-        action: Websocket.ClientActions.Subscribe,
-      };
-
-      const res = await ws.send(request);
-      expect(res).toEqual({
-        realm: Websocket.Realms.Notif,
-        type: Websocket.ServerMessageTypes.Response,
-        status: Websocket.ResponseStatuses.Success,
-        request: request,
-      });
-    });
-
-    it('subscribes to item with membership successfully', async () => {
-      const anna = await saveMember();
-      const { item } = await testUtils.saveItemAndMembership({ member: anna });
-      await testUtils.saveMembership({ item, member: actor });
-      const request = {
-        realm: Websocket.Realms.Notif,
-        topic: itemTopic,
-        channel: item.id,
-        action: Websocket.ClientActions.Subscribe,
-      };
-
-      const res = await ws.send(request);
-      expect(res).toEqual({
-        realm: Websocket.Realms.Notif,
-        type: Websocket.ServerMessageTypes.Response,
-        status: Websocket.ResponseStatuses.Success,
-        request: request,
-      });
-    });
-
-    it('cannot subscribe to item with no membership', async () => {
-      const anna = await saveMember();
-      const { item } = await testUtils.saveItemAndMembership({ member: anna });
-      const request = {
-        realm: Websocket.Realms.Notif,
-        topic: itemTopic,
-        channel: item.id,
-        action: Websocket.ClientActions.Subscribe,
-      };
-
-      const res = await ws.send(request);
-      expect(res).toEqual({
-        realm: Websocket.Realms.Notif,
-        type: Websocket.ServerMessageTypes.Response,
-        status: Websocket.ResponseStatuses.Error,
-        request: request,
-        error: new MemberCannotAccess(item.id),
-      });
-    });
-  });
-
-  describe('on copy item', () => {
-    it('parent item receives child create update', async () => {
-      const { item: item1 } = await testUtils.saveItemAndMembership({ member: actor });
-      const { item: item2 } = await testUtils.saveItemAndMembership({ member: actor });
-
-      const itemUpdates = await ws.subscribe({ topic: itemTopic, channel: item1.id });
-
-      const response = await app.inject({
-        method: HttpMethod.Post,
-        url: `/items/copy?id=${item2.id}`,
-        payload: { parentId: item1.id },
-      });
-      expect(response.statusCode).toBe(StatusCodes.ACCEPTED);
-
-      await waitForExpect(async () => {
-        expect(await testUtils.rawItemRepository.count()).toBeGreaterThan(2);
-      });
-
-      const copy = (await testUtils.itemRepository.getDescendants(item1 as FolderItem))[0];
-
-      await waitForExpect(() => {
-        const [childCreate] = itemUpdates;
-        expect(childCreate).toMatchObject(
-          ChildItemEvent('create', parseStringToDate(copy) as Item),
-        );
-      });
-    });
-
-    it('creator receives own items create update when destination is root', async () => {
-      const { item } = await testUtils.saveItemAndMembership({ member: actor });
-
-      const itemUpdates = await ws.subscribe({ topic: memberItemsTopic, channel: actor.id });
-
-      const response = await app.inject({
-        method: HttpMethod.Post,
-        url: `/items/copy?id=${item.id}`,
-        payload: {},
-      });
-      expect(response.statusCode).toBe(StatusCodes.ACCEPTED);
-
-      await waitForExpect(async () => {
-        expect(await testUtils.rawItemRepository.count()).toBeGreaterThan(1);
-      });
-
-      const copy = await testUtils.rawItemRepository.findOne({ where: { id: Not(item.id) } });
-
-      await waitForExpect(() => {
-        const [childCreate, accessibleCreate] = itemUpdates;
-        expect(childCreate).toMatchObject(OwnItemsEvent('create', parseStringToDate(copy) as Item));
-        expect(accessibleCreate).toMatchObject(
-          AccessibleItemsEvent('create', parseStringToDate(copy) as Item),
-        );
-      });
-    });
-  });
-
-  describe('on move item', () => {
-    it('parent of old location receives child deletion update', async () => {
-      const { item: oldParentItem } = await testUtils.saveItemAndMembership({ member: actor });
-      const { item: newParentItem } = await testUtils.saveItemAndMembership({ member: actor });
-      const { item: childItem } = await testUtils.saveItemAndMembership({
-        member: actor,
-        parentItem: oldParentItem,
-      });
-
-      const itemUpdates = await ws.subscribe({ topic: itemTopic, channel: oldParentItem.id });
-
-      const response = await app.inject({
-        method: HttpMethod.Post,
-        url: `/items/move?id=${childItem.id}`,
-        payload: { parentId: newParentItem.id },
-      });
-      expect(response.statusCode).toBe(StatusCodes.ACCEPTED);
-
-      await waitForExpect(async () => {
-        expect((await testUtils.rawItemRepository.findOneBy({ id: childItem.id }))?.path).toContain(
-          newParentItem.path,
-        );
-      });
-
-      await waitForExpect(() => {
-        const [childDelete] = itemUpdates;
-        expect(childDelete).toMatchObject(
-          ChildItemEvent('delete', parseStringToDate(childItem) as Item),
-        );
-      });
-    });
-
-    it('parent of new location receives child creation update', async () => {
-      const { item: oldParentItem } = await testUtils.saveItemAndMembership({ member: actor });
-      const { item: newParentItem } = await testUtils.saveItemAndMembership({ member: actor });
-      const { item: childItem } = await testUtils.saveItemAndMembership({
-        member: actor,
-        parentItem: oldParentItem,
-      });
-
-      const itemUpdates = await ws.subscribe({ topic: itemTopic, channel: newParentItem.id });
-
-      const response = await app.inject({
-        method: HttpMethod.Post,
-        url: `/items/move?id=${childItem.id}`,
-        payload: { parentId: newParentItem.id },
-      });
-      expect(response.statusCode).toBe(StatusCodes.ACCEPTED);
-
-      await waitForExpect(async () => {
-        expect((await testUtils.rawItemRepository.findOneBy({ id: childItem.id }))?.path).toContain(
-          newParentItem.path,
-        );
-      });
-
-      const moved = await testUtils.rawItemRepository.findOneBy({ id: childItem.id });
-
-      await waitForExpect(() => {
-        const [childCreate] = itemUpdates;
-        expect(childCreate).toMatchObject(
-          ChildItemEvent('create', parseStringToDate(moved) as Item),
-        );
-      });
-    });
-
-    it('creator receives own items delete update if old location was root of creator', async () => {
-      const { item: newParentItem } = await testUtils.saveItemAndMembership({ member: actor });
-      const { item } = await testUtils.saveItemAndMembership({
-        member: actor,
-      });
-
-      const itemUpdates = await ws.subscribe({ topic: memberItemsTopic, channel: actor.id });
-
-      const response = await app.inject({
-        method: HttpMethod.Post,
-        url: `/items/move?id=${item.id}`,
-        payload: { parentId: newParentItem.id },
-      });
-      expect(response.statusCode).toBe(StatusCodes.ACCEPTED);
-
-      await waitForExpect(async () => {
-        expect((await testUtils.rawItemRepository.findOneBy({ id: item.id }))?.path).toContain(
-          newParentItem.path,
-        );
-      });
-
-      await waitForExpect(() => {
-        const [ownDelete, accessibleDelete] = itemUpdates;
-        expect(ownDelete).toMatchObject(OwnItemsEvent('delete', parseStringToDate(item) as Item));
-        expect(accessibleDelete).toMatchObject(
-          AccessibleItemsEvent('delete', parseStringToDate(item) as Item),
-        );
-      });
-    });
-
-    it('creator receives own items create update if new location is root of creator', async () => {
-      const { item: oldParentItem } = await testUtils.saveItemAndMembership({ member: actor });
-      const { item: childItem } = await testUtils.saveItemAndMembership({
-        member: actor,
-        parentItem: oldParentItem,
-      });
-
-      const itemUpdates = await ws.subscribe({ topic: memberItemsTopic, channel: actor.id });
-
-      const response = await app.inject({
-        method: HttpMethod.Post,
-        url: `/items/move?id=${childItem.id}`,
-        payload: {},
-      });
-      expect(response.statusCode).toBe(StatusCodes.ACCEPTED);
-
-      await waitForExpect(async () => {
-        expect(
-          (await testUtils.rawItemRepository.findOneBy({ id: childItem.id }))?.path,
-        ).not.toContain(oldParentItem.path);
-      });
-
-      const moved = await testUtils.rawItemRepository.findOneBy({ id: childItem.id });
-
-      await waitForExpect(() => {
-        const [ownCreate] = itemUpdates;
-        expect(ownCreate).toMatchObject(OwnItemsEvent('create', parseStringToDate(moved) as Item));
-      });
-    });
   });
 
   describe('asynchronous feedback', () => {
@@ -347,7 +94,10 @@ describe('Item websocket hooks', () => {
 
     it('member that initiated the deleteMany operation receives success feedback', async () => {
       const { item } = await testUtils.saveItemAndMembership({ member: actor });
-      const memberUpdates = await ws.subscribe({ topic: memberItemsTopic, channel: actor.id });
+      const memberUpdates = await ws.subscribe<ItemEvent>({
+        topic: memberItemsTopic,
+        channel: actor.id,
+      });
 
       const response = await app.inject({
         method: HttpMethod.Delete,
@@ -360,7 +110,7 @@ describe('Item websocket hooks', () => {
       });
 
       await waitForExpect(() => {
-        const [_ownUpdate, _accessibleUpdate, feedbackUpdate] = memberUpdates;
+        const feedbackUpdate = memberUpdates.find((update) => update.kind === 'feedback');
         expect(feedbackUpdate).toMatchObject(
           ItemOpFeedbackEvent('delete', [item.id], { data: { [item.id]: item }, errors: [] }),
         );
@@ -369,7 +119,10 @@ describe('Item websocket hooks', () => {
 
     it('member that initiated the deleteMany operation receives failure feedback', async () => {
       const { item } = await testUtils.saveItemAndMembership({ member: actor });
-      const memberUpdates = await ws.subscribe({ topic: memberItemsTopic, channel: actor.id });
+      const memberUpdates = await ws.subscribe<ItemEvent>({
+        topic: memberItemsTopic,
+        channel: actor.id,
+      });
 
       jest.spyOn(ItemRepository.prototype, 'deleteMany').mockImplementation(() => {
         throw new Error('mock error');
@@ -382,7 +135,7 @@ describe('Item websocket hooks', () => {
       expect(response.statusCode).toBe(StatusCodes.ACCEPTED);
 
       await waitForExpect(() => {
-        const [_ownUpdate, _accessibleUpdate, feedbackUpdate] = memberUpdates;
+        const feedbackUpdate = memberUpdates.find((update) => update.kind === 'feedback');
         expect(feedbackUpdate).toMatchObject(
           ItemOpFeedbackEvent('delete', [item.id], { error: new Error('mock error') }),
         );
--- src/services/item/ws/hooks.ts
+++ src/services/item/ws/hooks.ts
@@ -1,57 +1,14 @@
 import { FastifyPluginAsync } from 'fastify';
 
-import { Websocket, getParentFromPath } from '@graasp/sdk';
+import { Websocket } from '@graasp/sdk';
 
-import { buildRepositories } from '../../../utils/repositories';
 import { WebsocketService } from '../../websockets/ws-service';
-import ItemService from '../service';
-import {
-  AccessibleItemsEvent,
-  ChildItemEvent,
-  OwnItemsEvent,
-  itemTopic,
-  memberItemsTopic,
-} from './events';
-
-/**
- * helper to register item topic
- */
-function registerItemTopic(websockets: WebsocketService, itemService: ItemService) {
-  websockets.register(itemTopic, async (req) => {
-    const { channel: id, member } = req;
-    await itemService.get(member, buildRepositories(), id);
-  });
-
-  // on copy item, notify destination parent of new child
-  itemService.hooks.setPostHook('copy', async (actor, repositories, { copy: item }) => {
-    const parentId = getParentFromPath(item.path);
-    if (parentId !== undefined) {
-      websockets.publish(itemTopic, parentId, ChildItemEvent('create', item));
-    }
-  });
-
-  // on move item, notify:
-  // - parent of old location of deleted child
-  // - parent of new location of new child
-  itemService.hooks.setPostHook(
-    'move',
-    async (actor, repositories, { sourceParentId, source, destination }) => {
-      if (sourceParentId !== undefined) {
-        websockets.publish(itemTopic, sourceParentId, ChildItemEvent('delete', source));
-      }
-
-      const destParentId = getParentFromPath(destination.path);
-      if (destParentId) {
-        websockets.publish(itemTopic, destParentId, ChildItemEvent('create', destination));
-      }
-    },
-  );
-}
+import { memberItemsTopic } from './events';
 
 /**
  * helper to register items of member topic
  */
-function registerMemberItemsTopic(websockets: WebsocketService, itemService: ItemService) {
+function registerMemberItemsTopic(websockets: WebsocketService) {
   websockets.register(memberItemsTopic, async (req) => {
     const { channel: memberId, member } = req;
     // requeted memberId channel must be current member
@@ -59,61 +16,12 @@ function registerMemberItemsTopic(websockets: WebsocketService, itemService: Ite
       throw new Websocket.AccessDeniedError();
     }
   });
-
-  // on copy, notify own items of creator with new item IF destination is root
-  itemService.hooks.setPostHook('copy', async (actor, repositories, { copy: item }) => {
-    const parentId = getParentFromPath(item.path);
-    if (parentId === undefined && item.creator) {
-      // root item, notify creator
-      // todo: remove own when we don't use own anymore
-      websockets.publish(memberItemsTopic, item.creator.id, OwnItemsEvent('create', item));
-      websockets.publish(memberItemsTopic, item.creator.id, AccessibleItemsEvent('create', item));
-    }
-  });
-
-  // on move item:
-  // - notify own items of creator of delete IF old location was root
-  // - notify own items of creator of create IF new location is root
-  itemService.hooks.setPostHook(
-    'move',
-    async (actor, repositories, { source, destination, sourceParentId }) => {
-      if (sourceParentId === undefined && source.creator) {
-        // root item, notify creator
-
-        // todo: remove own when we don't use own anymore
-        websockets.publish(memberItemsTopic, source.creator.id, OwnItemsEvent('delete', source));
-
-        websockets.publish(
-          memberItemsTopic,
-          source.creator.id,
-          AccessibleItemsEvent('delete', source),
-        );
-      }
-
-      const destParentId = getParentFromPath(destination.path);
-      if (destParentId === undefined && destination.creator) {
-        // root item, notify creator
-        // todo: remove own when we don't use own anymore
-        websockets.publish(
-          memberItemsTopic,
-          destination.creator.id,
-          OwnItemsEvent('create', destination),
-        );
-        websockets.publish(
-          memberItemsTopic,
-          destination.creator.id,
-          AccessibleItemsEvent('create', destination),
-        );
-      }
-    },
-  );
 }
 
 /**
  * Registers real-time websocket events for the item service
  */
 export const itemWsHooks: FastifyPluginAsync = async (fastify) => {
-  const { websockets, items } = fastify;
-  registerItemTopic(websockets, items.service);
-  registerMemberItemsTopic(websockets, items.service);
+  const { websockets } = fastify;
+  registerMemberItemsTopic(websockets);
 };
