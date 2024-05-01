cce0017fbc0ac035313257c6ba283eaddc7f1588
Thibault Reidy <thibault.reidy@hotmail.ch>
2024-05-01 13:58:50 +0000

    feat: extract link metadata in another function and implement checkEmbeddingAllowed

--- src/services/item/plugins/embeddedLink/index.ts
+++ src/services/item/plugins/embeddedLink/index.ts
@@ -1,5 +1,3 @@
-import fetch from 'node-fetch';
-
 import { FastifyPluginAsync } from 'fastify';
 
 import { ItemType } from '@graasp/sdk';
@@ -8,25 +6,12 @@ import { Repositories } from '../../../../utils/repositories';
 import { Actor } from '../../../member/entities/member';
 import { Item } from '../../entities/Item';
 import { createSchema } from './schemas';
+import { getLinkMetadata } from './utils';
 
 interface GraaspEmbeddedLinkItemOptions {
   /** \<protocol\>://\<hostname\>:\<port\> */
   iframelyHrefOrigin: string;
 }
-
-type IframelyLink = {
-  rel: string[];
-  href: string;
-};
-
-type IframelyResponse = {
-  meta: {
-    title?: string;
-    description?: string;
-  };
-  html: string;
-  links: IframelyLink[];
-};
 
 const plugin: FastifyPluginAsync<GraaspEmbeddedLinkItemOptions> = async (fastify, options) => {
   const { iframelyHrefOrigin } = options;
@@ -53,30 +38,24 @@ const plugin: FastifyPluginAsync<GraaspEmbeddedLinkItemOptions> = async (fastify
     const { embeddedLink } = item.extra;
 
     const { url } = embeddedLink;
-
-    const response = await fetch(`${iframelyHrefOrigin}/iframely?uri=${encodeURIComponent(url)}`);
-    // better clues on how to extract the metadata here: https://iframely.com/docs/links
-    const { meta = {}, html, links = [] } = (await response.json()) as IframelyResponse;
-    const { title, description } = meta;
+    const { title, description, html, thumbnails, icons } = await getLinkMetadata(
+      iframelyHrefOrigin,
+      url,
+    );
 
     // TODO: maybe all the code below should be moved to another place if it gets more complex
     if (title) {
-      item.name = title.trim();
+      item.name = title;
     }
     if (description) {
-      item.description = description.trim();
+      item.description = description;
     }
     if (html) {
       embeddedLink.html = html;
     }
 
-    embeddedLink.thumbnails = links
-      .filter(({ rel }) => hasThumbnailRel(rel))
-      .map(({ href }) => href);
-
-    embeddedLink.icons = links
-      .filter(({ rel }: { rel: string[] }) => hasIconRel(rel))
-      .map(({ href }) => href);
+    embeddedLink.thumbnails = thumbnails;
+    embeddedLink.icons = icons;
 
     // default settings
     item.settings = {
@@ -88,9 +67,5 @@ const plugin: FastifyPluginAsync<GraaspEmbeddedLinkItemOptions> = async (fastify
 
   itemService.hooks.setPreHook('create', hook);
 };
-
-const hasRel = (rel: string[], value: string) => rel.some((r) => r === value);
-const hasThumbnailRel = (rel: string[]) => hasRel(rel, 'thumbnail');
-const hasIconRel = (rel: string[]) => hasRel(rel, 'icon');
 
 export default plugin;
