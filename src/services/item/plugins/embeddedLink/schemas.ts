4732e4dd83a2d3878c3e2982b2115ebb48a26a16
spaenleh <spaenleh@gmail.com>
2024-05-24 09:28:58 +0200

    fix: make comments clearer in schema

--- src/services/item/plugins/embeddedLink/schemas.ts
+++ src/services/item/plugins/embeddedLink/schemas.ts
@@ -4,28 +4,29 @@ import { ItemType } from '@graasp/sdk';
 
 import { error } from '../../../../schemas/fluent-schema';
 
+// on link creation or update, the only allowed property in extra is the "url" property.
+// all other extra properties are filled by the backend.
+const strictUrlProperty = S.object()
+  .additionalProperties(false)
+  .prop('url', S.string().format('url'))
+  .required(['url']);
+
+// schema defining the properties that can be supplied on the "extra" for the link item on creation
 const embeddedLinkItemExtraCreate = S.object()
   .additionalProperties(false)
-  .prop(
-    ItemType.LINK,
-    // only allow to set url in extra on create
-    S.object().additionalProperties(false).prop('url', S.string().format('url')).required(['url']),
-  )
+  .prop(ItemType.LINK, strictUrlProperty)
   .required([ItemType.LINK]);
 
+// schema upgrading the general item creation schema, specifying the link properties allowed on creation
 export const createSchema = S.object()
   .prop('type', S.const(ItemType.LINK))
   .prop('extra', embeddedLinkItemExtraCreate)
   .required(['type', 'extra']);
 
-// schema defining the allowed properties for the link item
+// schema defining the properties that can be supplied on the "extra" for the link item on update
 export const updateExtraSchema = S.object()
   .additionalProperties(false)
-  .prop(
-    ItemType.LINK,
-    // only allow to set url in extra on create
-    S.object().additionalProperties(false).prop('url', S.string().format('url')).required(['url']),
-  )
+  .prop(ItemType.LINK, strictUrlProperty)
   .required([ItemType.LINK]);
 
 export const getLinkMetadata = {
