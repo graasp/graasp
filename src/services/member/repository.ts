144139cb04bb46a0fb2a4273c27ba965094c2975
kim <kim.phanhoang@epfl.ch>
2024-05-27 16:24:11 +0000

    refactor: refactor member repo and add test

--- src/services/member/repository.ts
+++ src/services/member/repository.ts
@@ -1,4 +1,4 @@
-import { In } from 'typeorm';
+import { EntityManager, In, Repository } from 'typeorm';
 
 import { UUID } from '@graasp/sdk';
 
@@ -7,12 +7,22 @@ import { MemberNotFound } from '../../utils/errors';
 import { mapById } from '../utils';
 import { Member } from './entities/member';
 
-export const MemberRepository = AppDataSource.getRepository(Member).extend({
+export class MemberRepository {
+  private repository: Repository<Member>;
+
+  constructor(manager?: EntityManager) {
+    if (manager) {
+      this.repository = manager.getRepository(Member);
+    } else {
+      this.repository = AppDataSource.getRepository(Member);
+    }
+  }
+
   async deleteOne(id: string) {
     // TODO:
     // check member exists
-    return this.delete(id);
-  },
+    return this.repository.delete(id);
+  }
 
   async get(id: string): Promise<Member> {
     // additional check that id is not null
@@ -20,25 +30,25 @@ export const MemberRepository = AppDataSource.getRepository(Member).extend({
     if (!id) {
       throw new MemberNotFound(id);
     }
-    const m = await this.findOneBy({ id });
+    const m = await this.repository.findOneBy({ id });
     if (!m) {
       throw new MemberNotFound(id);
     }
     return m;
-  },
+  }
 
   async getMany(ids: string[]) {
-    const members = await this.find({ where: { id: In(ids) } });
+    const members = await this.repository.find({ where: { id: In(ids) } });
     return mapById({
       keys: ids,
       findElement: (id) => members.find(({ id: thisId }) => thisId === id),
       buildError: (id) => new MemberNotFound(id),
     });
-  },
+  }
 
   async getByEmail(emailString: string, args: { shouldExist?: boolean } = {}) {
     const email = emailString.toLowerCase();
-    const member = await this.findOneBy({ email });
+    const member = await this.repository.findOneBy({ email });
 
     if (args.shouldExist) {
       if (!member) {
@@ -46,16 +56,17 @@ export const MemberRepository = AppDataSource.getRepository(Member).extend({
       }
     }
     return member;
-  },
+  }
 
   async getManyByEmail(emails: string[]) {
-    const members = await this.find({ where: { email: In(emails) } });
+    const members = await this.repository.find({ where: { email: In(emails) } });
+    console.log(members);
     return mapById({
       keys: emails,
       findElement: (email) => members.find(({ email: thisEmail }) => thisEmail === email),
       buildError: (email) => new MemberNotFound(email),
     });
-  },
+  }
 
   async patch(
     id: UUID,
@@ -82,11 +93,11 @@ export const MemberRepository = AppDataSource.getRepository(Member).extend({
     // TODO: throw if newData is empty
 
     // TODO: check member exists
-    await this.update(id, newData);
+    await this.repository.update(id, newData);
 
     // todo: optimize?
     return this.get(id);
-  },
+  }
 
   async post(data: Partial<Member> & Pick<Member, 'email'>): Promise<Member> {
     const email = data.email.toLowerCase();
@@ -96,7 +107,7 @@ export const MemberRepository = AppDataSource.getRepository(Member).extend({
     // The frontend avoids sending agreement data to prevent manipulation of the agreement date.
     // The agreements links are included in the registration email as a reminder.
     const userAgreementsDate = new Date();
-    const createdMember = await this.insert({
+    const createdMember = await this.repository.insert({
       ...data,
       email,
       userAgreementsDate,
@@ -105,5 +116,5 @@ export const MemberRepository = AppDataSource.getRepository(Member).extend({
     // TODO: better solution?
     // query builder returns creator as id and extra as string
     return this.get(createdMember.identifiers[0].id);
-  },
-});
+  }
+}
