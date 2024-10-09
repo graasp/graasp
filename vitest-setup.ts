aa1748f307053c15c25b4f1d7f73f73de30a9cb4
kim <kim.phanhoang@epfl.ch>
2024-10-09 12:25:00 +0000

    refactor: remove vitest setup

--- vitest-setup.ts
+++
@@ -1,5 +0,0 @@
-// to be able to use jest dom for transition
-// import '@testing-library/jest-dom/vitest';
-// needed to use decorators for Dependency Injection
-// should not be reimported in any other files !
-import 'reflect-metadata';
