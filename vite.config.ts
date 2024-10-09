2428e8c0e62e47a78a104008850589562d463cd2
kim <kim.phanhoang@epfl.ch>
2024-10-09 15:27:59 +0000

    feat: transpile to make di work in vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import typescript from '@rollup/plugin-typescript';
 import { URL, fileURLToPath } from 'url';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  plugins: [typescript({ tsconfig: './tsconfig.vitest.json' })],
+
   test: {
     include: ['**/*.vitest.ts'],
     coverage: {
