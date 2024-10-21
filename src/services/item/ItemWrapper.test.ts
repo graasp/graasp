a4b62f3633d18f4586c93f2077d69d0caef532dd
kim <kim.phanhoang@epfl.ch>
2024-10-21 10:35:17 +0000

    fix: order for createPackedItems

--- src/services/item/ItemWrapper.test.ts
+++ src/services/item/ItemWrapper.test.ts
@@ -1,13 +1,23 @@
+import { describe } from 'node:test';
+
 import { FastifyInstance } from 'fastify';
 
 import { ItemTagType } from '@graasp/sdk';
 
-import build, { clearDatabase } from '../../../test/app';
+import build, { MOCK_LOGGER, clearDatabase } from '../../../test/app';
+import { AppDataSource } from '../../plugins/datasource';
+import { buildRepositories } from '../../utils/repositories';
+import { saveMember } from '../member/test/fixtures/members';
+import { ThumbnailService } from '../thumbnail/service';
 import { ItemWrapper } from './ItemWrapper';
-import { createTag } from './plugins/itemTag/test/fixtures';
+import { ItemTag } from './plugins/itemTag/ItemTag';
+import { createTag, setItemPublic } from './plugins/itemTag/test/fixtures';
+import { ItemThumbnailService } from './plugins/thumbnail/service';
+import { ItemService } from './service';
 import { ItemTestUtils } from './test/fixtures/items';
 
 const testUtils = new ItemTestUtils();
+const rawItemTagRepository = AppDataSource.getRepository(ItemTag);
 
 describe('ItemWrapper', () => {
   let app: FastifyInstance;
@@ -25,20 +35,54 @@ describe('ItemWrapper', () => {
     jest.clearAllMocks();
   });
 
-  it('Return highest tags for child item', async () => {
-    const parentItem = testUtils.createItem();
-    const item = testUtils.createItem({ parentItem });
+  describe('createPackedItems', () => {
+    it('Return highest tags for child item', async () => {
+      const repositories = buildRepositories();
+      const itemThumbnailService = new ItemThumbnailService(
+        {} as unknown as ItemService,
+        {} as unknown as ThumbnailService,
+        MOCK_LOGGER,
+      );
+      jest.spyOn(itemThumbnailService, 'getUrlsByItems').mockImplementation(async () => ({}));
 
-    const publicTag = await createTag({ item, type: ItemTagType.Public });
-    const parentHiddenTag = await createTag({ item: parentItem, type: ItemTagType.Hidden });
-    const hiddenTag = await createTag({ item, type: ItemTagType.Hidden });
-    // unordered tags
-    const tags = [hiddenTag, publicTag, parentHiddenTag];
-    const itemWrapper = new ItemWrapper(item, undefined, tags);
+      const actor = await saveMember();
+      const { item: parentItem } = await testUtils.saveItemAndMembership({});
+      const { item } = await testUtils.saveItemAndMembership({ parentItem });
 
-    const packedItem = itemWrapper.packed();
-    expect(packedItem.public!.id).toEqual(publicTag.id);
-    // should return parent tag, not item tag
-    expect(packedItem.hidden!.id).toEqual(parentHiddenTag.id);
+      const hiddenTag = await rawItemTagRepository.save(
+        await createTag({ item, type: ItemTagType.Hidden }),
+      );
+      const parentPublicTag = await setItemPublic(parentItem);
+      await setItemPublic(item);
+
+      const [packedItem] = await ItemWrapper.createPackedItems(
+        actor,
+        repositories,
+        itemThumbnailService,
+        [item],
+      );
+      expect(packedItem.public!.id).toEqual(parentPublicTag.id);
+      // should return parent tag, not item tag
+      expect(packedItem.hidden!.id).toEqual(hiddenTag.id);
+    });
+  });
+
+  describe('packed', () => {
+    it('Return highest tags for child item', async () => {
+      const parentItem = testUtils.createItem();
+      const item = testUtils.createItem({ parentItem });
+
+      const publicTag = await createTag({ item, type: ItemTagType.Public });
+      const parentHiddenTag = await createTag({ item: parentItem, type: ItemTagType.Hidden });
+      const hiddenTag = await createTag({ item, type: ItemTagType.Hidden });
+      // unordered tags
+      const tags = [hiddenTag, publicTag, parentHiddenTag];
+      const itemWrapper = new ItemWrapper(item, undefined, tags);
+
+      const packedItem = itemWrapper.packed();
+      expect(packedItem.public!.id).toEqual(publicTag.id);
+      // should return parent tag, not item tag
+      expect(packedItem.hidden!.id).toEqual(parentHiddenTag.id);
+    });
   });
 });
