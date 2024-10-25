143ee67ea2e8b448dfda8eeb58d86a693571fbd3
kim <kim.phanhoang@epfl.ch>
2024-10-25 14:24:45 +0200

    refactor: rename migration file

--- src/migrations/1729857146775-item-category-to-item.ts
+++ src/migrations/1729857146775-item-category-to-item.ts
@@ -4,7 +4,7 @@ export class Migrations1729857146775 implements MigrationInterface {
   name = 'language-category-to-item-1729857146775';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // we ignore english category
+    // we ignore english category because default item lang is already english
     for (const [lang, key] of [
       ['french', 'fr'],
       ['german', 'de'],
@@ -13,13 +13,17 @@ export class Migrations1729857146775 implements MigrationInterface {
     ]) {
       // change item lang based on category lang
       await queryRunner.query(
-        `update item set lang='${key}' 
-        where path IN (select item_path from item_category left join category on category_id = category.id  where category.name = '${lang}' )`,
+        `UPDATE item SET lang='${key}' 
+        WHERE path IN (
+          SELECT item_path FROM item_category 
+          LEFT JOIN category ON category_id = category.id
+          WHERE category.name = '${lang}'
+        )`,
       );
 
       // remove language category from db
       // cascade delete relatede item_category
-      await queryRunner.query(`DELETE category where type ='language'`);
+      await queryRunner.query(`DELETE FROM category where type='language'`);
     }
   }
 
