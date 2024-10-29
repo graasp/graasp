dc1b3a6de750147bc142939226351c10e3d2f1e3
spaenleh <spaenleh@gmail.com>
2024-10-29 09:33:42 +0000

    fix: issues

--- bootstrapDB.sql
+++ bootstrapDB.sql
@@ -1,5 +1,4 @@
 -- Umami
--- postgres://umami:umami@postgres:5432/umami
 create user umami with password 'umami';
 create database umami with owner umami;
 -- Backend
@@ -8,6 +7,6 @@ create database graasp with owner graasper;
 --Etherpad
 create user etherpad with password 'etherpad';
 create database etherpad with owner etherpad;
---Etherpad
+-- Test database
 create user test with password 'test';
 create database test with owner test;
