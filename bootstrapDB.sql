bf8e86a7690e2d1e52904706886c3c86d5b5b728
Basile Spaenlehauer <spaenleh@gmail.com>
2024-10-29 13:36:52 +0100

    fix: setup single db instance (#1575)
    * fix: setup single db instance
    
    * fix: issues
    
    * fix: review comments

---
+++ bootstrapDB.sql
@@ -0,0 +1,12 @@
+-- Umami
+create user umami with password 'umami';
+create database umami with owner umami;
+-- Backend
+create user graasper with password 'graasper';
+create database graasp with owner graasper;
+--Etherpad
+create user etherpad with password 'etherpad';
+create database etherpad with owner etherpad;
+-- Test database
+create user test with password 'test';
+create database test with owner test;
