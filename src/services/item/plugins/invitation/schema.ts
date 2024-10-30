99ada26b8580fd1ae5a34379213b62a09318b9f3
Kim Lan Phan Hoang <pyphilia@gmail.com>
2024-10-30 16:52:56 +0100

    docs: add docs for invitation endpoints (#1581)
    * docs: add docs for invitation endpoints
    
    * refactor: apply PR requested changes

--- src/services/item/plugins/invitation/schema.ts
+++ src/services/item/plugins/invitation/schema.ts
@@ -15,7 +15,6 @@ export const invitationSchemaRef = registerSchemaAsRef(
   'Invitation',
   Type.Object(
     {
-      // Object Definition
       id: customType.UUID(),
       email: Type.String({ format: 'email' }),
       name: Type.Optional(customType.Nullable(Type.String())),
@@ -25,84 +24,71 @@ export const invitationSchemaRef = registerSchemaAsRef(
       updatedAt: customType.DateTime(),
     },
     {
-      // Schema Options
+      description:
+        'Invitation for a non-registered user to access an item. The user is identified by email.',
       additionalProperties: false,
     },
-  ),
-);
-
-export const minimalInvitationSchemaRef = registerSchemaAsRef(
-  'minimalInvitation',
-  'Minimal Invitation',
-  Type.Object(
-    {
-      // Object Definition
-      email: Type.String({ format: 'email' }),
-      name: Type.Optional(customType.Nullable(Type.String())),
-      permission: Type.Enum(PermissionLevel),
-    },
-    {
-      // Schema Options
-      additionalProperties: false,
-    },
-  ),
-);
-
-export const updateInvitationSchemaRef = registerSchemaAsRef(
-  'updateInvitation',
-  'Update Invitation',
-  // Object Definition
-  Type.Object(
-    {
-      name: Type.Optional(Type.String()),
-      permission: Type.Optional(Type.Enum(PermissionLevel)),
-    },
-    { additionalProperties: true, minProperties: 1 },
   ),
 );
 
 export const invite = {
+  operationId: 'createInvitation',
+  tags: ['invitation'],
+  summary: 'Invite user by email to access an item',
+  description: 'Invite non-registered user with their email to access given item.',
+
   params: customType.StrictObject({
     id: customType.UUID(),
   }),
-  body: Type.Object(
-    {
-      invitations: Type.Array(
-        Type.Object({
-          email: Type.String({ format: 'email' }),
-          permission: Type.Enum(PermissionLevel),
-        }),
-      ),
-    },
-    { additionalProperties: false },
-  ),
+  body: customType.StrictObject({
+    invitations: Type.Array(
+      Type.Object({
+        email: Type.String({ format: 'email' }),
+        permission: Type.Enum(PermissionLevel),
+      }),
+    ),
+  }),
   response: {
-    [StatusCodes.OK]: {
-      type: 'object',
-      properties: {
+    [StatusCodes.OK]: customType.StrictObject(
+      {
         memberships: Type.Array(itemMembershipSchemaRef),
         invitations: Type.Array(invitationSchemaRef),
       },
-    },
+      { description: 'Successful Response' },
+    ),
     '4xx': errorSchemaRef,
     [StatusCodes.INTERNAL_SERVER_ERROR]: errorSchemaRef,
   },
 } as const satisfies FastifySchema;
 
 export const inviteFromCSV = {
+  operationId: 'inviteFromCSV',
+  tags: ['invitation'],
+  summary: 'Invite users from CSV file',
+  description: 'Invite users from CSV file, given their email, optional name and permission level.',
+
   params: customType.StrictObject({
     id: customType.UUID(),
   }),
   response: {
-    [StatusCodes.OK]: Type.Object({
-      memberships: Type.Array(itemMembershipSchemaRef),
-      invitations: Type.Array(invitationSchemaRef),
-    }),
+    [StatusCodes.OK]: Type.Object(
+      {
+        memberships: Type.Array(itemMembershipSchemaRef),
+        invitations: Type.Array(invitationSchemaRef),
+      },
+      { description: 'Successful Response' },
+    ),
     '4xx': errorSchemaRef,
   },
 } as const satisfies FastifySchema;
 
 export const inviteFromCSVWithTemplate = {
+  operationId: 'inviteFromCSVWithTemplate',
+  tags: ['invitation'],
+  summary: 'Invite users from CSV file in groups given a template',
+  description:
+    'Invite users by groups from CSV. This will create groups of users having access to corresponding resources from a given template.',
+
   params: customType.StrictObject({
     id: customType.UUID(),
   }),
@@ -116,22 +102,33 @@ export const inviteFromCSVWithTemplate = {
         memberships: Type.Array(itemMembershipSchemaRef),
         invitations: Type.Array(invitationSchemaRef),
       }),
+      { description: 'Successful Response' },
     ),
     '4xx': errorSchemaRef,
   },
 } as const satisfies FastifySchema;
 
 export const getForItem = {
+  operationId: 'getInvitationForItem',
+  tags: ['invitation'],
+  summary: 'Get invitations for a given item',
+  description: 'Get invitation for a given item',
+
   params: customType.StrictObject({
     id: customType.UUID(),
   }),
   response: {
-    [StatusCodes.OK]: Type.Array(invitationSchemaRef),
+    [StatusCodes.OK]: Type.Array(invitationSchemaRef, { description: 'Successful Response' }),
     '4xx': errorSchemaRef,
   },
 } as const satisfies FastifySchema;
 
 export const getById = {
+  operationId: 'getInvitationById',
+  tags: ['invitation'],
+  summary: 'Get invitation',
+  description: 'Get invitation by id',
+
   params: customType.StrictObject({
     id: customType.UUID(),
   }),
@@ -142,8 +139,19 @@ export const getById = {
 } as const satisfies FastifySchema;
 
 export const updateOne = {
+  operationId: 'updateInvitation',
+  tags: ['invitation'],
+  summary: 'Update invitation',
+  description: "Update invitation's name or permission",
+
   params: Type.Object({ id: customType.UUID(), invitationId: customType.UUID() }),
-  body: updateInvitationSchemaRef,
+  body: customType.StrictObject(
+    {
+      name: Type.Optional(Type.String()),
+      permission: Type.Optional(Type.Enum(PermissionLevel)),
+    },
+    { minProperties: 1 },
+  ),
   response: {
     [StatusCodes.OK]: invitationSchemaRef,
     '4xx': errorSchemaRef,
@@ -151,13 +159,27 @@ export const updateOne = {
 } as const satisfies FastifySchema;
 
 export const deleteOne = {
+  operationId: 'deleteInvitation',
+  tags: ['invitation'],
+  summary: 'Delete invitation',
+  description: 'Delete invitation',
+
   params: Type.Object({ id: customType.UUID(), invitationId: customType.UUID() }),
   response: {
-    [StatusCodes.OK]: Type.String(),
+    [StatusCodes.OK]: customType.UUID({ description: 'Successful Response' }),
     '4xx': errorSchemaRef,
   },
 } as const satisfies FastifySchema;
 
 export const sendOne = {
+  operationId: 'sendInvitation',
+  tags: ['invitation'],
+  summary: 'Send invitation',
+  description: 'Send invitation',
+
   params: Type.Object({ id: customType.UUID(), invitationId: customType.UUID() }),
+  response: {
+    [StatusCodes.NO_CONTENT]: Type.Null({ description: 'Successful Response' }),
+    '4xx': errorSchemaRef,
+  },
 } as const satisfies FastifySchema;
