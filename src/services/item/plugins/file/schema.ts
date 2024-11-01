6ff31b7e482052cc1f65380d058a46bf5ffca7a0
Kim Lan Phan Hoang <pyphilia@gmail.com>
2024-11-01 11:32:20 +0100

    docs: add documentation for file endpoints (#1585)
    * docs: add documentation for file endpoints
    
    * refactor: apply PR requested changes

--- src/services/item/plugins/file/schema.ts
+++ src/services/item/plugins/file/schema.ts
@@ -1,27 +1,47 @@
 import { Type } from '@sinclair/typebox';
+import { StatusCodes } from 'http-status-codes';
 
 import { customType } from '../../../../plugins/typebox';
+import { errorSchemaRef } from '../../../../schemas/global';
+import { itemSchemaRef } from '../../schemas';
 
 export const upload = {
-  querystring: Type.Partial(
-    Type.Object(
-      {
-        id: customType.UUID(),
-        previousItemId: customType.UUID(),
-      },
-      {
-        additionalProperties: false,
-      },
+  operationId: 'uploadFile',
+  tags: ['item', 'file'],
+  summary: 'Upload files',
+  description: 'Upload files to create corresponding items.',
+
+  querystring: customType.StrictObject({
+    id: Type.Optional(
+      customType.UUID({
+        description: 'Folder id in which the uploaded files should be created.',
+      }),
     ),
-  ),
+    previousItemId: Type.Optional(
+      customType.UUID({ description: 'The uploaded files should be created after this item.' }),
+    ),
+  }),
+  response: {
+    [StatusCodes.OK]: customType.StrictObject({
+      data: Type.Record(customType.UUID(), itemSchemaRef),
+      errors: Type.Array(errorSchemaRef),
+    }),
+    '4xx': errorSchemaRef,
+  },
 };
 
 export const download = {
+  operationId: 'downloadFile',
+  tags: ['item', 'file'],
+  summary: 'Download file',
+  description: 'Download file.',
+
   params: customType.StrictObject({
     id: customType.UUID(),
   }),
-  querystring: Type.Object(
-    { replyUrl: Type.Boolean({ default: false }) },
-    { additionalProperties: false },
-  ),
+  querystring: customType.StrictObject({ replyUrl: Type.Boolean({ default: false }) }),
+  response: {
+    [StatusCodes.OK]: Type.String({ format: 'uri' }),
+    '4xx': errorSchemaRef,
+  },
 };
