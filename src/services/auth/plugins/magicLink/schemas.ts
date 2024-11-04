53706c70d4eb4442bd1fdc1cd2d6e8d2ab9ee4c1
Kim Lan Phan Hoang <pyphilia@gmail.com>
2024-11-04 10:50:23 +0100

    docs: add documentation for magiclink (#1600)

--- src/services/auth/plugins/magicLink/schemas.ts
+++ src/services/auth/plugins/magicLink/schemas.ts
@@ -1,46 +1,65 @@
 import { Type } from '@sinclair/typebox';
+import { StatusCodes } from 'http-status-codes';
 
 import { FastifySchema } from 'fastify';
 
 import { customType } from '../../../../plugins/typebox';
+import { errorSchemaRef } from '../../../../schemas/global';
 import { SHORT_TOKEN_PARAM } from '../passport';
 
 export const register = {
-  body: Type.Object(
-    {
-      name: customType.Username(),
-      email: Type.String({ format: 'email' }),
-      captcha: Type.String(),
-      url: Type.Optional(Type.String({ format: 'uri' })),
-      enableSaveActions: Type.Optional(Type.Boolean()),
-    },
-    { additionalProperties: false },
-  ),
-  querystring: Type.Object(
-    {
-      lang: Type.Optional(Type.String()),
-    },
-    { additionalProperties: false },
-  ),
+  operationId: 'register',
+  tags: ['authentication'],
+  summary: 'Register with email and name',
+  description:
+    'Register with email and name, protected by a captcha. The captcha is used to prevent brute force attacks.',
+
+  body: customType.StrictObject({
+    name: customType.Username(),
+    email: Type.String({ format: 'email' }),
+    captcha: Type.String(),
+    url: Type.Optional(Type.String({ format: 'uri' })),
+    enableSaveActions: Type.Optional(Type.Boolean()),
+  }),
+  querystring: customType.StrictObject({
+    lang: Type.Optional(Type.String()),
+  }),
+  response: {
+    [StatusCodes.NO_CONTENT]: Type.Null({ description: 'Successful Response' }),
+    '4xx': errorSchemaRef,
+  },
 } as const satisfies FastifySchema;
 
 export const login = {
-  body: Type.Object(
-    {
-      email: Type.String({ format: 'email' }),
-      captcha: Type.String(),
-      url: Type.Optional(Type.String({ format: 'uri' })),
-    },
-    { additionalProperties: false },
-  ),
+  operationId: 'login',
+  tags: ['authentication'],
+  summary: 'Login with email',
+  description:
+    'Login with email, protected by a captcha. The captcha is used to prevent brute force attacks.',
+
+  body: customType.StrictObject({
+    email: Type.String({ format: 'email' }),
+    captcha: Type.String(),
+    url: Type.Optional(Type.String({ format: 'uri' })),
+  }),
+  response: {
+    [StatusCodes.NO_CONTENT]: Type.Null({ description: 'Successful Response' }),
+    '4xx': errorSchemaRef,
+  },
 } as const satisfies FastifySchema;
 
 export const auth = {
-  querystring: Type.Object(
-    {
-      [SHORT_TOKEN_PARAM]: Type.String({ format: 'jwt' }),
-      url: Type.Optional(Type.String({ format: 'uri' })),
-    },
-    { additionalProperties: false },
-  ),
+  operationId: 'authenticate',
+  tags: ['authentication'],
+  summary: 'Authentication validating the token',
+  description: 'Authenticate to obtain session cookie given provided token and verifier',
+
+  querystring: customType.StrictObject({
+    [SHORT_TOKEN_PARAM]: Type.String({ format: 'jwt' }),
+    url: Type.Optional(Type.String({ format: 'uri' })),
+  }),
+  response: {
+    [StatusCodes.SEE_OTHER]: Type.Null({ description: 'Successful Response' }),
+    '4xx': errorSchemaRef,
+  },
 } as const satisfies FastifySchema;
--- src/services/item/plugins/enroll/schema.ts
+++ src/services/item/plugins/enroll/schema.ts
@@ -4,6 +4,7 @@ import { customType } from '../../../../plugins/typebox';
 import { itemMembershipSchemaRef } from '../../../itemMembership/schemas';
 
 export const enroll = {
+  operationId: 'enroll',
   tags: ['itemMemberships'],
   summary: 'Create an item membership for the logged in user if there is an Item Login',
   description: `Create an item membership on the item with the given ID for the logged in user. 
