69516221f568f4f808586ee6650db21fbada94ad
kim <kim.phanhoang@epfl.ch>
2024-11-05 14:36:10 +0100

    refactor: fix migration

--- src/migrations/1730798658866-item-visibility.ts
+++ src/migrations/1730798658866-item-visibility.ts
@@ -6,19 +6,19 @@ export class Migrations1730798658866 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "item_tag" RENAME TO "item_visibility"`);
     await queryRunner.query(
-      `ALTER INDEX IDX_gist_item_tag_path RENAME TO IDX_gist_item_visibility_path`,
+      `ALTER INDEX "IDX_gist_item_tag_path" RENAME TO "IDX_gist_item_visibility_path"`,
     );
     await queryRunner.query(
-      `ALTER TABLE "item_visibility" RENAME CONSTRAINT item-tag TO UQ_item_visibility_item_type`,
+      `ALTER TABLE "item_visibility" RENAME CONSTRAINT "item-tag" TO "UQ_item_visibility_item_type"`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER INDEX IDX_gist_item_visibility_path RENAME TO IDX_gist_item_tag_path`,
+      `ALTER INDEX "IDX_gist_item_visibility_path" RENAME TO "IDX_gist_item_tag_path"`,
     );
     await queryRunner.query(
-      `ALTER TABLE "item_visibility" RENAME CONSTRAINT UQ_item_visibility_item_type TO item-tag`,
+      `ALTER TABLE "item_visibility" RENAME CONSTRAINT "UQ_item_visibility_item_type" TO "item-tag"`,
     );
     await queryRunner.query(`ALTER TABLE "item_visibility" RENAME TO "item_tag"`);
   }
