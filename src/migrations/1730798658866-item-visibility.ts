6fad5699741ea78221d3d1bf0e3e5b9cd72ca80e
kim <kim.phanhoang@epfl.ch>
2024-11-05 11:48:19 +0100

    refactor: fix migration

--- src/migrations/1730798658866-item-visibility.ts
+++ src/migrations/1730798658866-item-visibility.ts
@@ -4,7 +4,7 @@ export class Migrations1730798658866 implements MigrationInterface {
   name = 'item-visibility-1730798658866';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER "item_tag" RENAME TO "item_visibility"`);
+    await queryRunner.query(`ALTER TABLE "item_tag" RENAME TO "item_visibility"`);
     await queryRunner.query(`RENAME INDEX IDX_gist_item_tag_path TO IDX_gist_item_visibility_path`);
     await queryRunner.query(
       `ALTER TABLE "item_visibility" RENAME CONSTRAINT item-tag TO UQ_item_visibility_item_type`,
@@ -16,6 +16,6 @@ export class Migrations1730798658866 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "item_visibility" RENAME CONSTRAINT UQ_item_visibility_item_type TO item-tag`,
     );
-    await queryRunner.query(`ALTER "item_visibility" RENAME TO "item_tag"`);
+    await queryRunner.query(`ALTER TABLE "item_visibility" RENAME TO "item_tag"`);
   }
 }
