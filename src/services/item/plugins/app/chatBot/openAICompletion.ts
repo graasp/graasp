e1d5bfdb2e6c64de21b27159bf297ce3ae01e747
Jérémy La Scala <jeremy.lascala@epfl.ch>
2024-11-07 10:13:45 +0100

    chore(chatbot): remove redundant check for temperature param

--- src/services/item/plugins/app/chatBot/openAICompletion.ts
+++ src/services/item/plugins/app/chatBot/openAICompletion.ts
@@ -2,13 +2,8 @@ import { OpenAI } from 'openai';
 
 import { ChatBotMessage, GPTVersion } from '@graasp/sdk';
 
-import {
-  OPENAI_API_KEY,
-  OPENAI_MAX_TEMP,
-  OPENAI_MIN_TEMP,
-  OPENAI_ORG_ID,
-} from '../../../../../utils/config';
-import { OpenAIBadTemperature, OpenAIBadVersion } from '../../../../../utils/errors';
+import { OPENAI_API_KEY, OPENAI_ORG_ID } from '../../../../../utils/config';
+import { OpenAIBadVersion } from '../../../../../utils/errors';
 
 export const openAICompletion = async (
   body: Array<ChatBotMessage>,
@@ -29,14 +24,6 @@ export const openAICompletion = async (
 
   return completion;
 };
-
-export function validateTemperature(temperature?: number) {
-  if (temperature) {
-    if (temperature < OPENAI_MIN_TEMP || temperature > OPENAI_MAX_TEMP) {
-      throw new OpenAIBadTemperature(temperature, OPENAI_MAX_TEMP, OPENAI_MIN_TEMP);
-    }
-  }
-}
 
 export function validateGPTVersion(gptVersion?: GPTVersion) {
   if (gptVersion && !Object.values(GPTVersion).includes(gptVersion as GPTVersion)) {
