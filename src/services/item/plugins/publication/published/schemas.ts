be60d239627034194288c9f6b34a34fbeaffbc22
kim <kim.phanhoang@epfl.ch>
2024-11-14 12:41:44 +0000

    fix: add documentation and details to published schemas

--- src/services/item/plugins/publication/published/schemas.ts
+++ src/services/item/plugins/publication/published/schemas.ts
@@ -9,12 +9,11 @@ import {
   GET_MOST_LIKED_ITEMS_MAXIMUM,
   GET_MOST_RECENT_ITEMS_MAXIMUM,
 } from '../../../../../utils/config';
-import { LIST_OF_UUID_V4_REGEX_PATTERN } from '../../../../../utils/constants';
 import { nullableMemberSchemaRef } from '../../../../member/schemas';
 import { itemSchemaRef } from '../../../schemas';
 import { packedItemSchemaRef } from '../../../schemas.packed';
 
-const publishEntry = Type.Object(
+const publishEntry = customType.StrictObject(
   {
     id: customType.UUID(),
     item: itemSchemaRef,
@@ -22,112 +21,109 @@ const publishEntry = Type.Object(
     createdAt: customType.DateTime(),
   },
   {
-    additionalProperties: false,
+    description: 'Information of a published item',
   },
 );
-
-const publishEntryWithViews = Type.Object(
-  {
-    id: customType.UUID(),
-    item: itemSchemaRef,
-    creator: nullableMemberSchemaRef,
-    createdAt: customType.DateTime(),
-    totalViews: Type.Number(),
-  },
-  {
-    additionalProperties: false,
-  },
-);
-
-export const getCollections = {
-  querystring: {
-    type: 'object',
-    properties: {
-      categoryId: {
-        type: 'array',
-        items: Type.String({
-          pattern: LIST_OF_UUID_V4_REGEX_PATTERN,
-        }),
-        maxItems: MAX_TARGETS_FOR_READ_REQUEST,
-      },
-    },
-  },
-
-  response: {
-    [StatusCodes.OK]: Type.Array(itemSchemaRef),
-  },
-};
 
 export const getRecentCollections = {
-  querystring: Type.Object(
-    {
-      limit: Type.Number({
-        maximum: GET_MOST_RECENT_ITEMS_MAXIMUM,
-        minimum: 1,
-        default: GET_MOST_RECENT_ITEMS_MAXIMUM,
-      }),
-    },
-    { additionalProperties: false },
-  ),
+  operationId: 'getRecentCollections',
+  tags: ['collection'],
+  summary: 'Get most recent published items',
+  description: 'Get most recent published items (collections)',
+
+  querystring: customType.StrictObject({
+    limit: Type.Number({
+      maximum: GET_MOST_RECENT_ITEMS_MAXIMUM,
+      minimum: 1,
+      default: GET_MOST_RECENT_ITEMS_MAXIMUM,
+    }),
+  }),
 
   response: {
     [StatusCodes.OK]: Type.Array(itemSchemaRef),
+    '4xx': errorSchemaRef,
   },
 };
 
 export const getMostLikedItems = {
-  querystring: Type.Object(
-    {
-      limit: Type.Number({
-        maximum: GET_MOST_LIKED_ITEMS_MAXIMUM,
-        minimum: 1,
-        default: GET_MOST_LIKED_ITEMS_MAXIMUM,
-      }),
-    },
-    { additionalProperties: false },
-  ),
+  operationId: 'getMostLikedCollections',
+  tags: ['collection'],
+  summary: 'Get most liked items',
+  description: 'Get most liked items (collections)',
+
+  querystring: customType.StrictObject({
+    limit: Type.Number({
+      maximum: GET_MOST_LIKED_ITEMS_MAXIMUM,
+      minimum: 1,
+      default: GET_MOST_LIKED_ITEMS_MAXIMUM,
+    }),
+  }),
   response: {
     [StatusCodes.OK]: Type.Array(itemSchemaRef),
+    '4xx': errorSchemaRef,
   },
 };
 
 export const getCollectionsForMember = {
-  params: Type.Object(
-    {
-      memberId: customType.UUID(),
-    },
-    { additionalProperties: false },
-  ),
+  operationId: 'getCollectionsForMember',
+  tags: ['collection'],
+  summary: 'Get collections for member',
+  description: 'Get collections for member.',
+
+  params: customType.StrictObject({
+    memberId: customType.UUID(),
+  }),
 
   response: {
     [StatusCodes.OK]: Type.Array(packedItemSchemaRef),
+    '4xx': errorSchemaRef,
   },
 };
 
 export const publishItem = {
+  operationId: 'publishItem',
+  tags: ['collection'],
+  summary: 'Publish an item',
+  description: 'Publish an item, that will become listed in the Library.',
+
   params: customType.StrictObject({
     itemId: customType.UUID(),
   }),
   response: {
     [StatusCodes.OK]: publishEntry,
+    '4xx': errorSchemaRef,
   },
 };
 
 export const unpublishItem = {
+  operationId: 'unpublishItem',
+  tags: ['collection'],
+  summary: 'Unpublish an item',
+  description: 'Unpublish an item, that will not be available anymore on the Library.',
+
   params: customType.StrictObject({
     itemId: customType.UUID(),
   }),
   response: {
     [StatusCodes.OK]: publishEntry,
+    '4xx': errorSchemaRef,
   },
 };
 
 export const getInformations = {
+  operationId: 'getCollectionInformations',
+  tags: ['collection'],
+  summary: 'Get information of a collection',
+  description: 'Get information of a collection, including views count.',
+
   params: customType.StrictObject({
     itemId: customType.UUID(),
   }),
   response: {
-    [StatusCodes.OK]: customType.Nullable(publishEntryWithViews),
+    [StatusCodes.OK]: customType.Nullable(
+      Type.Composite([publishEntry, customType.StrictObject({ totalViews: Type.Number() })]),
+    ),
+    '4xx': errorSchemaRef,
   },
 };
 
