d103a965adfab097d533146ce5459b961a1ca1ab
spaenleh <spaenleh@gmail.com>
2024-11-28 09:19:44 +0000

    fix: use lower limit for consecutive exports

--- src/services/action/constants/constants.ts
+++ src/services/action/constants/constants.ts
@@ -7,6 +7,6 @@ export const MAX_ACTIONS_SAMPLE_SIZE = 10000;
 
 export const ZIP_MIMETYPE = 'application/zip';
 
-export const DEFAULT_REQUEST_EXPORT_INTERVAL = 3600 * 1000 * 24; // 1 day - used for timestamp
+export const DEFAULT_REQUEST_EXPORT_INTERVAL = 3600 * 1000 * 2; // 2hours - used for timestamp
 export const EXPORT_FILE_EXPIRATION_DAYS = 7;
 export const EXPORT_FILE_EXPIRATION = 3600 * 24 * EXPORT_FILE_EXPIRATION_DAYS; // max value: one week
