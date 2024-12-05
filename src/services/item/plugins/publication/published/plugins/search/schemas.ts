ab9a65857667e1a43abcce4b2cd4ca5aedd1c691
kim <kim.phanhoang@epfl.ch>
2024-12-05 14:32:48 +0000

    refactor: integrate search filters logic in backend

--- src/services/item/plugins/publication/published/plugins/search/schemas.ts
+++ src/services/item/plugins/publication/published/plugins/search/schemas.ts
@@ -3,6 +3,8 @@ import { StatusCodes } from 'http-status-codes';
 
 import { FastifySchema } from 'fastify';
 
+import { ItemType, TagCategory } from '@graasp/sdk';
+
 import { customType } from '../../../../../../../plugins/typebox';
 import { errorSchemaRef } from '../../../../../../../schemas/global';
 
@@ -13,38 +15,74 @@ export const search = {
   description:
     'Get collections given search query. the results are highlighted given the search query.',
 
-  body: customType.StrictObject({
-    queries: Type.Array(
-      Type.Composite(
-        [
-          customType.StrictObject({
-            indexUid: Type.String(),
-          }),
-          Type.Partial(
-            Type.Object(
-              {
-                attributesToHighlight: Type.Array(Type.String()),
-                attributesToCrop: Type.Array(Type.String()),
-                cropLength: Type.Number(),
-                q: Type.String(),
-                page: Type.Number(),
-                limit: Type.Number(),
-                sort: Type.Array(Type.String()),
-                filter: Type.String(),
-                highlightPreTag: Type.String(),
-                highlightPostTag: Type.String(),
-              },
-              {
-                additionalProperties: true,
-              },
-            ),
+  body: Type.Partial(
+    customType.StrictObject({
+      attributesToHighlight: Type.Array(Type.String()),
+      attributesToCrop: Type.Array(Type.String()),
+      cropLength: Type.Number(),
+      q: Type.String(),
+      page: Type.Number(),
+      limit: Type.Number(),
+      sort: Type.Array(Type.String()),
+      highlightPreTag: Type.String(),
+      highlightPostTag: Type.String(),
+      // custom filter props
+      tags: Type.Partial(Type.Record(Type.Enum(TagCategory), Type.Array(Type.String()))),
+      langs: Type.Array(Type.String()),
+      isPublishedRoot: Type.Boolean(),
+    }),
+  ),
+  response: {
+    [StatusCodes.OK]: customType.StrictObject({
+      results: Type.Array(
+        customType.StrictObject({
+          totalHits: Type.Optional(Type.Number()),
+          estimatedTotalHits: Type.Optional(Type.Number()),
+          hits: Type.Array(
+            customType.StrictObject({
+              name: Type.String(),
+              description: Type.String(),
+              content: Type.String(),
+              creator: customType.StrictObject({
+                id: customType.UUID(),
+                name: Type.String(),
+              }),
+              level: Type.Array(Type.String()),
+              discipline: Type.Array(Type.String()),
+              'resource-type': Type.Array(Type.String()),
+              id: customType.UUID(),
+              type: Type.Enum(ItemType),
+              isPublishedRoot: Type.Boolean(),
+              isHidden: Type.Boolean(),
+              createdAt: customType.DateTime(),
+              updatedAt: customType.DateTime(),
+              lang: Type.String(),
+              _formatted: customType.StrictObject({
+                name: Type.String(),
+                description: Type.String(),
+                content: Type.String(),
+                creator: customType.StrictObject({
+                  id: customType.UUID(),
+                  name: Type.String(),
+                }),
+                level: Type.Array(Type.String()),
+                discipline: Type.Array(Type.String()),
+                'resource-type': Type.Array(Type.String()),
+                id: customType.UUID(),
+                type: Type.Enum(ItemType),
+                isPublishedRoot: Type.Boolean(),
+                isHidden: Type.Boolean(),
+                createdAt: customType.DateTime(),
+                updatedAt: customType.DateTime(),
+                lang: Type.String(),
+              }),
+            }),
           ),
-        ],
-        { additionalProperties: false },
+        }),
       ),
-    ),
-  }),
-  response: {},
+    }),
+    '4xx': errorSchemaRef,
+  },
 } as const satisfies FastifySchema;
 
 export const getFacets = {
