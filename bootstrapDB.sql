782657ebd02f10234e2bee4042eeb7a967afa2a6
kim <kim.phanhoang@epfl.ch>
2024-12-13 16:14:04 +0100

    refactor: apply PR requested changes

--- bootstrapDB.sql
+++ bootstrapDB.sql
@@ -13,5 +13,5 @@ create database test with owner test;
 
 -- set timeout settings for postgres
 -- should avoid transactions to hang for too long
-set idle_in_transaction_session_timeout = '3600000';
-set statement_timeout = '3600000';
+set idle_in_transaction_session_timeout = '3600000'; -- in milliseconds, 1h
+set statement_timeout = '3600000'; -- in milliseconds, 1h
