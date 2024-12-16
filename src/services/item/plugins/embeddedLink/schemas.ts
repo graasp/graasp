d1a807842c86f9967c6b4db2a0ce15aaf4016e0b
kim <kim.phanhoang@epfl.ch>
2024-12-16 17:18:23 +0100

    feat: add post and patch links

--- src/services/item/plugins/embeddedLink/schemas.ts
+++ src/services/item/plugins/embeddedLink/schemas.ts
@@ -5,6 +5,29 @@ import { FastifySchema } from 'fastify';
 
 import { customType } from '../../../../plugins/typebox';
 import { errorSchemaRef } from '../../../../schemas/global';
+import { itemSchema } from '../../schemas';
+import { geoCoordinateSchemaRef } from '../geolocation/schemas';
+
+export const embeddedLinkSchema = Type.Composite([
+  itemSchema,
+  customType.StrictObject(
+    {
+      extra: customType.StrictObject({
+        embeddedLink: customType.StrictObject({
+          url: Type.String({ format: 'uri' }),
+        }),
+      }),
+      settings: customType.StrictObject({
+        showLinkIframe: Type.Optional(Type.Boolean()),
+        showLinkButton: Type.Optional(Type.Boolean()),
+      }),
+    },
+    {
+      title: 'Embedded Link',
+      description: 'Item of type embedded link, represents a resource to an external website.',
+    },
+  ),
+]);
 
 export const getLinkMetadata = {
   operationId: 'getLinkMetadata',
@@ -26,4 +49,43 @@ export const getLinkMetadata = {
     }),
     '4xx': errorSchemaRef,
   },
+} as const satisfies FastifySchema;
+
+export const createLink = {
+  operationId: 'createLink',
+  tags: ['item', 'link'],
+  summary: 'Create link',
+  description: 'Create link.',
+
+  querystring: Type.Partial(
+    customType.StrictObject({ parentId: customType.UUID(), previousItemId: customType.UUID() }),
+  ),
+  body: Type.Composite([
+    customType.StrictObject({ url: Type.String({ format: 'uri' }) }),
+    Type.Pick(embeddedLinkSchema, ['name']),
+    Type.Partial(Type.Pick(embeddedLinkSchema, ['description', 'lang', 'settings'])),
+    customType.StrictObject({
+      geolocation: Type.Optional(geoCoordinateSchemaRef),
+    }),
+  ]),
+  response: { [StatusCodes.OK]: embeddedLinkSchema, '4xx': errorSchemaRef },
+} as const satisfies FastifySchema;
+
+export const updateLink = {
+  operationId: 'updateLink',
+  tags: ['item'],
+  summary: 'Update link',
+  description: 'Update link given body.',
+
+  params: customType.StrictObject({
+    id: customType.UUID(),
+  }),
+  body: Type.Partial(
+    Type.Composite([
+      Type.Pick(embeddedLinkSchema, ['name', 'description', 'lang', 'settings']),
+      customType.StrictObject({ url: Type.String() }),
+    ]),
+    { minProperties: 1 },
+  ),
+  response: { [StatusCodes.OK]: embeddedLinkSchema, '4xx': errorSchemaRef },
 } as const satisfies FastifySchema;
