4ec8b4b8aed26045dc455dfb859eb3a3b1721d96
kim <kim.phanhoang@epfl.ch>
2024-12-19 16:36:00 +0100

    fix: fix tests

--- src/services/item/test/fixtures/items.ts
+++ src/services/item/test/fixtures/items.ts
@@ -20,7 +20,7 @@ import {
 import { AppDataSource } from '../../../../plugins/datasource';
 import { Guest } from '../../../itemLogin/entities/guest';
 import { ItemMembership } from '../../../itemMembership/entities/ItemMembership';
-import { Member } from '../../../member/entities/member';
+import { Member, isMember } from '../../../member/entities/member';
 import { saveMember } from '../../../member/test/fixtures/members';
 import { ItemWrapper, PackedItem } from '../../ItemWrapper';
 import { DEFAULT_ORDER, Item, ItemExtraMap } from '../../entities/Item';
@@ -170,18 +170,23 @@ export class ItemTestUtils {
     itemMembership: ItemMembership;
     packedItem: PackedItem;
   }> => {
-    const { item, permission, creator, parentItem } = options;
-    let member = options.member instanceof Member ? options.member : null;
+    const { item, permission, creator = null, parentItem, member } = options;
+    // use given creator, or member if it is a member
+    let itemCreator = creator;
+    if (!creator && member && isMember(member)) {
+      itemCreator = member;
+    }
+
     const newItem = await this.saveItem({
       item,
-      actor: creator ?? member,
+      actor: itemCreator,
       parentItem,
     });
-    if (!member) {
-      member = await saveMember();
-    }
-    const account: Member | Guest = member;
-    const im = await this.saveMembership({ item: newItem, account, permission });
+    const im = await this.saveMembership({
+      item: newItem,
+      account: member ?? (await saveMember()),
+      permission,
+    });
     return {
       item: newItem,
       itemMembership: im,
