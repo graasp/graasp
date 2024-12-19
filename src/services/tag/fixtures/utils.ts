043f8076b5880f407cff3cae177a809f6b688ca0
kim <kim.phanhoang@epfl.ch>
2024-12-19 17:54:26 +0100

    refactor: add comment --no-verify

--- src/services/tag/fixtures/utils.ts
+++ src/services/tag/fixtures/utils.ts
@@ -6,6 +6,12 @@ import { Tag } from '../Tag.entity';
 
 const tagRawRepository = AppDataSource.getRepository(Tag);
 
+/**
+ * Add and return a tag, does not throw in case of duplicata
+ * It is safe to catch errors when this function is used to set up a test
+ * @param args
+ * @returns
+ */
 export async function saveTag(args: { name?: string; category?: TagCategory } = {}) {
   const tag = TagFactory(args);
   try {
