5090bf52a2d06d2cbf3cd0f5efd94fc36d85d4d0
kim <kim.phanhoang@epfl.ch>
2025-01-06 17:35:27 +0100

    refactor: refactor tests

--- src/services/caching/service.test.ts
+++ src/services/caching/service.test.ts
@@ -19,15 +19,17 @@ describe('CachingService Tests', () => {
     await cache.delete(MOCKED_PATH);
   };
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     ({ app } = await build());
     cache = resolveDependency(FILE_SERVICE_URLS_CACHING_DI_KEY);
   });
 
+  afterAll(() => {
+    app.close();
+  });
   afterEach(async () => {
     await resetValidations();
     jest.clearAllMocks();
-    app.close();
   });
 
   it('Empty cache returns given URL once, but then returns the cached URL', async () => {
