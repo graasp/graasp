afa624927caf6b7ccd47e0f812d75b7a4e430411
kim <kim.phanhoang@epfl.ch>
2025-01-21 11:12:08 +0100

    refactor: add tests

--- src/services/item/plugins/publication/published/repositories/itemPublished.test.ts
+++ src/services/item/plugins/publication/published/repositories/itemPublished.test.ts
@@ -1,36 +1,76 @@
-import { FastifyInstance } from 'fastify';
+import { DataSource, Repository } from 'typeorm';
 
-import build, { clearDatabase } from '../../../../../../../test/app';
-import { saveMember } from '../../../../../member/test/fixtures/members';
-import { ItemTestUtils, expectManyItems } from '../../../../test/fixtures/items';
+import { FolderItemFactory, MemberFactory, PermissionLevel } from '@graasp/sdk';
+
+import { AppDataSource } from '../../../../../../plugins/datasource';
+import { ItemMembership } from '../../../../../itemMembership/entities/ItemMembership';
+import { Member } from '../../../../../member/entities/member';
+import { Item } from '../../../../entities/Item';
+import { expectManyItems } from '../../../../test/fixtures/items';
+import { ItemPublished } from '../entities/itemPublished';
 import { ItemPublishedRepository } from './itemPublished';
 
-const itemPublishedRepository = new ItemPublishedRepository();
-const testUtils = new ItemTestUtils();
-
 describe('ItemPublishedRepository', () => {
-  let app: FastifyInstance;
-  let actor;
+  let db: DataSource;
+  let repository: ItemPublishedRepository;
+  let rawRepository: Repository<ItemPublished>;
+  let itemRawRepository: Repository<Item>;
+  let memberRawRepository: Repository<Member>;
+  let itemMembershipRawRepository: Repository<ItemMembership>;
 
-  beforeEach(async () => {
-    ({ app, actor } = await build());
+  beforeAll(async () => {
+    db = await AppDataSource.initialize();
+    await db.runMigrations();
+    repository = new ItemPublishedRepository(db.manager);
+    rawRepository = db.getRepository(ItemPublished);
+    itemRawRepository = db.getRepository(Item);
+    memberRawRepository = db.getRepository(Member);
+    itemMembershipRawRepository = db.getRepository(ItemMembership);
   });
-  afterEach(async () => {
-    jest.clearAllMocks();
-    await clearDatabase(app.db);
-    actor = null;
-    app.close();
+
+  afterAll(async () => {
+    await db.dropDatabase();
+    await db.destroy();
   });
 
   describe('getForMember', () => {
     it('get published items for member', async () => {
-      const { items } = await testUtils.saveCollections(actor);
-      // noise
-      const member = await saveMember();
-      await testUtils.saveCollections(member);
+      const creator = await memberRawRepository.save(MemberFactory());
+      const items = [
+        await itemRawRepository.save(FolderItemFactory({ creator })),
+        await itemRawRepository.save(FolderItemFactory({ creator })),
+        await itemRawRepository.save(FolderItemFactory({ creator })),
+      ];
+      for (const i of items) {
+        await itemMembershipRawRepository.save({
+          item: { path: i.path },
+          account: { id: creator.id },
+          permission: PermissionLevel.Admin,
+        });
+        await rawRepository.save({
+          item: { path: i.path },
+        });
+      }
 
-      const result = await itemPublishedRepository.getForMember(actor.id);
+      const result = await repository.getForMember(creator.id);
       expectManyItems(result, items);
+    });
+  });
+
+  describe('touchUpdatedAt', () => {
+    it('undefined path throws', async () => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      await expect(() => repository.touchUpdatedAt(undefined)).rejects.toThrow();
+    });
+    it('update updatedAt on current time', async () => {
+      const updatedAt = new Date();
+      const creator = await memberRawRepository.save(MemberFactory());
+      const item = await itemRawRepository.save(FolderItemFactory({ creator }));
+
+      const result = await repository.touchUpdatedAt(item.path);
+
+      expect(new Date(result).getTime() - new Date(updatedAt).getTime()).toBeLessThanOrEqual(100);
     });
   });
 });
