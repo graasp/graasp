284b7081dc3bd632f02e0f761c004265609d941c
kim <kim.phanhoang@epfl.ch>
2025-02-28 12:01:20 +0100

    test: update seed for item visibility

--- src/services/item/plugins/itemVisibility/test/index.test.ts
+++ src/services/item/plugins/itemVisibility/test/index.test.ts
@@ -10,10 +10,9 @@ import build, {
   mockAuthenticate,
   unmockAuthenticate,
 } from '../../../../../../test/app';
+import { seedFromJson } from '../../../../../../test/mocks/seed';
 import { AppDataSource } from '../../../../../plugins/datasource';
 import { ITEMS_ROUTE_PREFIX } from '../../../../../utils/config';
-import { saveMember } from '../../../../member/test/fixtures/members';
-import { ItemTestUtils } from '../../../test/fixtures/items';
 import { ItemVisibility } from '../ItemVisibility';
 import {
   CannotModifyParentVisibility,
@@ -21,7 +20,6 @@ import {
   ItemVisibilityNotFound,
 } from '../errors';
 
-const testUtils = new ItemTestUtils();
 const rawItemTagRepository = AppDataSource.getRepository(ItemVisibility);
 
 export const saveTagsForItem = async ({ item, creator }) => {
@@ -35,7 +33,6 @@ export const saveTagsForItem = async ({ item, creator }) => {
 
 describe('Item Visibility', () => {
   let app: FastifyInstance;
-  let actor;
 
   beforeAll(async () => {
     ({ app } = await build({ member: null }));
@@ -48,22 +45,19 @@ describe('Item Visibility', () => {
 
   afterEach(async () => {
     jest.clearAllMocks();
-    actor = null;
     unmockAuthenticate();
   });
 
   describe('POST /:itemId/visibilities', () => {
-    let item;
-    const type = ItemVisibilityType.Hidden;
-
     describe('Signed Out', () => {
       it('Throws if item is private', async () => {
-        const member = await saveMember();
-        ({ item } = await testUtils.saveItemAndMembership({ member }));
+        const {
+          items: [item],
+        } = await seedFromJson({ actor: null, items: [{}] });
 
         const response = await app.inject({
           method: HttpMethod.Post,
-          url: `${ITEMS_ROUTE_PREFIX}/${item.id}/visibilities/${type}`,
+          url: `${ITEMS_ROUTE_PREFIX}/${item.id}/visibilities/${ItemVisibilityType.Hidden}`,
         });
 
         expect(response.statusCode).toEqual(StatusCodes.UNAUTHORIZED);
@@ -71,30 +65,34 @@ describe('Item Visibility', () => {
     });
 
     describe('Signed In', () => {
-      beforeEach(async () => {
-        actor = await saveMember();
-        mockAuthenticate(actor);
-      });
-
       it('Create a visibility for an item', async () => {
-        ({ item } = await testUtils.saveItemAndMembership({ member: actor }));
+        const {
+          actor,
+          items: [item],
+        } = await seedFromJson({ items: [{ memberships: [{ account: 'actor' }] }] });
+        mockAuthenticate(actor);
 
         const res = await app.inject({
           method: HttpMethod.Post,
-          url: `${ITEMS_ROUTE_PREFIX}/${item.id}/visibilities/${type}`,
+          url: `${ITEMS_ROUTE_PREFIX}/${item.id}/visibilities/${ItemVisibilityType.Hidden}`,
         });
         expect(res.statusCode).toBe(StatusCodes.OK);
-        expect(res.json().type).toEqual(type);
+        expect(res.json().type).toEqual(ItemVisibilityType.Hidden);
         expect(res.json().item.path).toEqual(item.path);
       });
 
       it('Cannot create visibility if exists for item', async () => {
-        ({ item } = await testUtils.saveItemAndMembership({ member: actor }));
-        await rawItemTagRepository.save({ item, type, creator: actor });
+        const {
+          actor,
+          items: [item],
+        } = await seedFromJson({
+          items: [{ isHidden: true, memberships: [{ account: 'actor' }] }],
+        });
+        mockAuthenticate(actor);
 
         const res = await app.inject({
           method: HttpMethod.Post,
-          url: `${ITEMS_ROUTE_PREFIX}/${item.id}/visibilities/${type}`,
+          url: `${ITEMS_ROUTE_PREFIX}/${item.id}/visibilities/${ItemVisibilityType.Hidden}`,
         });
         expect(res.json()).toMatchObject(
           new ConflictingVisibilitiesInTheHierarchy(expect.anything()),
@@ -102,13 +100,17 @@ describe('Item Visibility', () => {
       });
 
       it('Cannot create visibility if exists on parent', async () => {
-        const { item: parent } = await testUtils.saveItemAndMembership({ member: actor });
-        ({ item } = await testUtils.saveItemAndMembership({ member: actor, parentItem: parent }));
-        await rawItemTagRepository.save({ item: parent, type, creator: actor });
+        const {
+          actor,
+          items: [_parentItem, child],
+        } = await seedFromJson({
+          items: [{ isHidden: true, memberships: [{ account: 'actor' }], children: [{}] }],
+        });
+        mockAuthenticate(actor);
 
         const res = await app.inject({
           method: HttpMethod.Post,
-          url: `${ITEMS_ROUTE_PREFIX}/${item.id}/visibilities/${type}`,
+          url: `${ITEMS_ROUTE_PREFIX}/${child.id}/visibilities/${ItemVisibilityType.Hidden}`,
         });
         expect(res.json()).toMatchObject(
           new ConflictingVisibilitiesInTheHierarchy(expect.anything()),
@@ -118,7 +120,7 @@ describe('Item Visibility', () => {
       it('Bad request if item id is invalid', async () => {
         const res = await app.inject({
           method: HttpMethod.Post,
-          url: `${ITEMS_ROUTE_PREFIX}/invalid-id/visibilities/${type}`,
+          url: `${ITEMS_ROUTE_PREFIX}/invalid-id/visibilities/${ItemVisibilityType.Hidden}`,
         });
         expect(res.statusCode).toBe(StatusCodes.BAD_REQUEST);
       });
@@ -139,92 +141,111 @@ describe('Item Visibility', () => {
         expect(res.statusCode).toBe(StatusCodes.BAD_REQUEST);
       });
     });
-  });
+    // });
 
-  describe('DELETE /:itemId/visibilities/:id', () => {
-    let item, itemVisibilities;
-    const type = ItemVisibilityType.Public;
+    describe('DELETE /:itemId/visibilities/:id', () => {
+      describe('Signed Out', () => {
+        it('Throws if item is private', async () => {
+          const {
+            items: [item],
+          } = await seedFromJson({ actor: null, items: [{}] });
 
-    describe('Signed Out', () => {
-      it('Throws if item is private', async () => {
-        const member = await saveMember();
-        ({ item } = await testUtils.saveItemAndMembership({ member }));
+          const response = await app.inject({
+            method: HttpMethod.Delete,
+            url: `${ITEMS_ROUTE_PREFIX}/${item.id}/visibilities/${ItemVisibilityType.Public}`,
+          });
 
-        const response = await app.inject({
-          method: HttpMethod.Delete,
-          url: `${ITEMS_ROUTE_PREFIX}/${v4()}/visibilities/${type}`,
+          expect(response.statusCode).toEqual(StatusCodes.UNAUTHORIZED);
         });
-
-        expect(response.statusCode).toEqual(StatusCodes.UNAUTHORIZED);
-      });
-    });
-
-    describe('Signed In', () => {
-      let toDelete;
-
-      beforeEach(async () => {
-        actor = await saveMember();
-        mockAuthenticate(actor);
-
-        ({ item } = await testUtils.saveItemAndMembership({ member: actor }));
-        itemVisibilities = await saveTagsForItem({ item, creator: actor });
-        toDelete = itemVisibilities[0];
       });
 
-      it('Delete a visibility of an item (and descendants)', async () => {
-        const { item: child } = await testUtils.saveItemAndMembership({
-          member: actor,
-          parentItem: item,
-        });
-        const childTags = await saveTagsForItem({ item: child, creator: actor });
-        const descendantToDelete = childTags.find(({ type }) => type === toDelete.type);
+      describe('Signed In', () => {
+        it('Delete a visibility of an item (and descendants)', async () => {
+          const {
+            actor,
+            items: [parentItem],
+            itemVisibilities: [parentPublicVisibility, childPublicVisibility],
+          } = await seedFromJson({
+            items: [
+              {
+                isPublic: true,
+                memberships: [{ account: 'actor' }],
+                children: [{ isPublic: true }],
+              },
+            ],
+          });
+          mockAuthenticate(actor);
 
-        const res = await app.inject({
-          method: HttpMethod.Delete,
-          url: `${ITEMS_ROUTE_PREFIX}/${item.id}/visibilities/${toDelete.type}`,
-        });
+          const res = await app.inject({
+            method: HttpMethod.Delete,
+            url: `${ITEMS_ROUTE_PREFIX}/${parentItem.id}/visibilities/${ItemVisibilityType.Public}`,
+          });
 
-        expect(res.statusCode).toBe(StatusCodes.OK);
-        expect(res.json().item.path).toEqual(item.path);
-        const itemVisibility = await rawItemTagRepository.findOneBy({ id: toDelete.id });
-        expect(itemVisibility).toBeNull();
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const childItemTag = await rawItemTagRepository.findOneBy({ id: descendantToDelete!.id });
-        expect(childItemTag).toBeNull();
-      });
-      it('Cannot delete inherited visibility', async () => {
-        const { item: parent } = await testUtils.saveItemAndMembership({ member: actor });
-        ({ item } = await testUtils.saveItemAndMembership({ member: actor, parentItem: parent }));
-        const visibility = await rawItemTagRepository.save({ item: parent, type, creator: actor });
+          expect(res.statusCode).toBe(StatusCodes.OK);
+          expect(res.json().item.path).toEqual(parentItem.path);
+          const itemVisibility = await rawItemTagRepository.findOneBy({
+            id: parentPublicVisibility.id,
+          });
+          expect(itemVisibility).toBeNull();
+          const childItemTag = await rawItemTagRepository.findOneBy({
+            id: childPublicVisibility.id,
+          });
+          expect(childItemTag).toBeNull();
+        });
+        it('Cannot delete inherited visibility', async () => {
+          const {
+            actor,
+            items: [_parentItem, child],
+          } = await seedFromJson({
+            items: [
+              {
+                isPublic: true,
+                memberships: [{ account: 'actor' }],
+                children: [{}],
+              },
+            ],
+          });
+          mockAuthenticate(actor);
 
-        const res = await app.inject({
-          method: HttpMethod.Delete,
-          url: `${ITEMS_ROUTE_PREFIX}/${item.id}/visibilities/${visibility.type}`,
+          const res = await app.inject({
+            method: HttpMethod.Delete,
+            url: `${ITEMS_ROUTE_PREFIX}/${child.id}/visibilities/${ItemVisibilityType.Public}`,
+          });
+          expect(res.json()).toMatchObject(new CannotModifyParentVisibility(expect.anything()));
         });
-        expect(res.json()).toMatchObject(new CannotModifyParentVisibility(expect.anything()));
-      });
-      it('Throws if visibility does not exist', async () => {
-        const { item: itemWithoutTag } = await testUtils.saveItemAndMembership({ member: actor });
+        it('Throws if visibility does not exist', async () => {
+          const {
+            actor,
+            items: [item],
+          } = await seedFromJson({
+            items: [
+              {
+                memberships: [{ account: 'actor' }],
+              },
+            ],
+          });
+          mockAuthenticate(actor);
 
-        const res = await app.inject({
-          method: HttpMethod.Delete,
-          url: `${ITEMS_ROUTE_PREFIX}/${itemWithoutTag.id}/visibilities/${ItemVisibilityType.Hidden}`,
+          const res = await app.inject({
+            method: HttpMethod.Delete,
+            url: `${ITEMS_ROUTE_PREFIX}/${item.id}/visibilities/${ItemVisibilityType.Hidden}`,
+          });
+          expect(res.json()).toMatchObject(new ItemVisibilityNotFound(expect.anything()));
         });
-        expect(res.json()).toMatchObject(new ItemVisibilityNotFound(expect.anything()));
-      });
-      it('Bad request if item id is invalid', async () => {
-        const res = await app.inject({
-          method: HttpMethod.Delete,
-          url: `${ITEMS_ROUTE_PREFIX}/invalid-id/visibilities/${ItemVisibilityType.Hidden}`,
+        it('Bad request if item id is invalid', async () => {
+          const res = await app.inject({
+            method: HttpMethod.Delete,
+            url: `${ITEMS_ROUTE_PREFIX}/invalid-id/visibilities/${ItemVisibilityType.Hidden}`,
+          });
+          expect(res.statusCode).toBe(StatusCodes.BAD_REQUEST);
         });
-        expect(res.statusCode).toBe(StatusCodes.BAD_REQUEST);
-      });
-      it('Bad request if item visibility id is invalid', async () => {
-        const res = await app.inject({
-          method: HttpMethod.Delete,
-          url: `${ITEMS_ROUTE_PREFIX}/${v4()}/visibilities/invalid-id`,
+        it('Bad request if item visibility id is invalid', async () => {
+          const res = await app.inject({
+            method: HttpMethod.Delete,
+            url: `${ITEMS_ROUTE_PREFIX}/${v4()}/visibilities/invalid-id`,
+          });
+          expect(res.statusCode).toBe(StatusCodes.BAD_REQUEST);
         });
-        expect(res.statusCode).toBe(StatusCodes.BAD_REQUEST);
       });
     });
   });
