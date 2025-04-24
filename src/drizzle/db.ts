2dca2dc9e26a6cba41b39574390a91675a8f8f64
spaenleh <spaenleh@gmail.com>
2025-04-24 12:07:52 +0000

    fix: read replica env vars

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { withReplicas } from 'drizzle-orm/pg-core';
 import { Pool } from 'pg';
 
-import { DB_CONNECTION_POOL_SIZE, DB_READ_REPLICA_HOSTS } from '../utils/config';
+import { DB_CONNECTION_POOL_SIZE, DB_READ_REPLICA_CONNECTIONS } from '../utils/config';
 import * as relations from './relations';
 import * as schema from './schema';
 
@@ -18,14 +18,10 @@ export const primaryDb = drizzle({
   logger: process.env.DATABASE_LOGS === 'true',
 });
 
-const readReplica = DB_READ_REPLICA_HOSTS[0]
+const readReplica = DB_READ_REPLICA_CONNECTIONS[0]
   ? drizzle({
       client: new Pool({
-        host: DB_READ_REPLICA_HOSTS[0],
-        port: Number(process.env.DB_PORT),
-        user: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        connectionString: DB_READ_REPLICA_CONNECTIONS[0],
         max: DB_CONNECTION_POOL_SIZE,
       }),
       schema: { ...schema, ...relations },
