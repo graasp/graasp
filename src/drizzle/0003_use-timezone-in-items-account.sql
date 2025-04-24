d75b79962abb2f24772bf6f1b97dd7cc66bbaeb3
kim <kim.phanhoang@epfl.ch>
2025-04-24 11:07:32 +0200

    refactor: refactor migration sql

--- src/drizzle/0003_use-timezone-in-items-account.sql
+++ src/drizzle/0003_use-timezone-in-items-account.sql
@@ -1,18 +1,15 @@
--- Alter column types to use timestamp with timezone
+-- Need to drop the views before changing the column types 
+DROP view item_view;--> statement-breakpoint
+DROP view guests_view;--> statement-breakpoint
+DROP view members_view;--> statement-breakpoint
 
+-- Alter column types to use timestamp with timezone
 ALTER TABLE membership_request ALTER COLUMN "created_at" type timestamptz USING created_at AT TIME ZONE 'UTC';--> statement-breakpoint
 
--- Need to drop the view before changing the column types
-DROP view item_view;--> statement-breakpoint
 ALTER TABLE item ALTER COLUMN "created_at" type timestamptz USING created_at AT TIME ZONE 'UTC';--> statement-breakpoint
 ALTER TABLE item ALTER COLUMN "updated_at" type timestamptz USING updated_at AT TIME ZONE 'UTC';--> statement-breakpoint
 ALTER TABLE item ALTER COLUMN "deleted_at" type timestamptz USING deleted_at AT TIME ZONE 'UTC';--> statement-breakpoint
--- Re-create the item view after the column type change
-CREATE VIEW "public"."item_view" AS (select "id", "name", "type", "description", "path", "creator_id", "extra", "settings", "created_at", "updated_at", "lang", "order" from "item" where "item"."deleted_at" is null);--> statement-breakpoint
 
--- Need to drop the guests and members view before changing the column types
-Drop view guests_view;--> statement-breakpoint
-Drop view members_view;--> statement-breakpoint
 ALTER TABLE account ALTER COLUMN created_at type timestamptz USING created_at AT TIME ZONE 'UTC';--> statement-breakpoint
 ALTER TABLE account ALTER COLUMN "updated_at" type timestamptz USING updated_at AT TIME ZONE 'UTC';--> statement-breakpoint
 ALTER TABLE account ALTER COLUMN user_agreements_date type timestamptz USING user_agreements_date AT TIME ZONE 'UTC';--> statement-breakpoint
@@ -83,7 +80,8 @@ ALTER TABLE "recycled_item_data" ALTER COLUMN "created_at" type timestamptz USIN
 ALTER TABLE "short_link" ALTER COLUMN "created_at" type timestamptz USING created_at AT TIME ZONE 'UTC';--> statement-breakpoint
 
 
--- Re-create the guest and member views after the column type change
+-- Re-create the views after the column type change
+CREATE VIEW "public"."item_view" AS (select "id", "name", "type", "description", "path", "creator_id", "extra", "settings", "created_at", "updated_at", "lang", "order" from "item" where "item"."deleted_at" is null);--> statement-breakpoint
 CREATE VIEW "public"."guests_view" AS (select "id", "name", "extra", "type", "created_at", "updated_at", "last_authenticated_at", "is_validated", "item_login_schema_id" from "account" where ("account"."type" = 'guest' and "account"."item_login_schema_id" is not null));--> statement-breakpoint
 CREATE VIEW "public"."members_view" AS (select "id", "name", "email", "extra", "type", "created_at", "updated_at", "user_agreements_date", "enable_save_actions", "last_authenticated_at", "is_validated" from "account" where ("account"."type" = 'individual' and "account"."email" is not null));--> statement-breakpoint
 
