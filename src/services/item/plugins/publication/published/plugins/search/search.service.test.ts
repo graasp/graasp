11e001067e081513b6516c7510b4f7eedaf3703e
spaenleh <spaenleh@gmail.com>
2025-06-02 06:27:08 +0000

    fix: limit changed name:

--- src/services/item/plugins/publication/published/plugins/search/search.service.test.ts
+++ src/services/item/plugins/publication/published/plugins/search/search.service.test.ts
@@ -141,9 +141,9 @@ describe('getMostLiked', () => {
     const results = await searchService.getMostLiked(4);
     expect(results).toEqual(MOCK_RESULT);
 
-    const { sort, limit } = spy.mock.calls[0][0].queries[0];
+    const { sort, hitsPerPage } = spy.mock.calls[0][0].queries[0];
     expect(sort).toEqual(['likes:desc']);
-    expect(limit).toEqual(4);
+    expect(hitsPerPage).toEqual(4);
   });
 });
 
@@ -160,9 +160,9 @@ describe('getMostRecent', () => {
     const results = await searchService.getMostRecent(4);
     expect(results).toEqual(MOCK_RESULT);
 
-    const { sort, limit } = spy.mock.calls[0][0].queries[0];
+    const { sort, hitsPerPage } = spy.mock.calls[0][0].queries[0];
     expect(sort).toEqual(['publicationUpdatedAt:desc']);
-    expect(limit).toEqual(4);
+    expect(hitsPerPage).toEqual(4);
   });
 });
 
