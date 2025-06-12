f050d693de8e11cd526db5353ffc4f0d139ec795
spaenleh <spaenleh@gmail.com>
2025-06-12 13:25:13 +0000

    fix: do not expose the bashboard when mode is not dev

--- src/workers/dashboard.controller.ts
+++ src/workers/dashboard.controller.ts
@@ -5,19 +5,25 @@ import { Queue } from 'bullmq';
 
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 
+import { DEV } from '../config/env';
 import { REDIS_CONNECTION } from '../config/redis';
 import { QueueNames } from './config';
 
 export const queueDashboardPlugin: FastifyPluginAsyncTypebox = async (instance) => {
-  const serverAdapter = new FastifyAdapter();
+  if (DEV) {
+    const serverAdapter = new FastifyAdapter();
 
-  const queues = [new Queue(QueueNames.ItemExport, { connection: { url: REDIS_CONNECTION } })];
+    const queues = [new Queue(QueueNames.ItemExport, { connection: { url: REDIS_CONNECTION } })];
 
-  createBullBoard({
-    queues: queues.map((q) => new BullMQAdapter(q)),
-    serverAdapter,
-  });
+    createBullBoard({
+      queues: queues.map((q) => new BullMQAdapter(q)),
+      serverAdapter,
+    });
 
-  serverAdapter.setBasePath('/ui');
-  instance.register(serverAdapter.registerPlugin(), { prefix: '/ui' });
+    serverAdapter.setBasePath('/ui');
+    instance.register(serverAdapter.registerPlugin(), { prefix: '/ui' });
+  } else {
+    // warn that the dahsboard will not be available.
+    instance.log.info('Not running in dev mode, the bullmq dashboard will not be available.');
+  }
 };
