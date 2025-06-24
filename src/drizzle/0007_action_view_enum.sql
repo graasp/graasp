048582186e2fa6605ca019bb9b07d926d13b3331
spaenleh <spaenleh@gmail.com>
2025-06-24 12:43:57 +0000

    fix: add migration to remove explorer value

---
+++ src/drizzle/0007_action_view_enum.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "action" ALTER COLUMN "view" SET DATA TYPE text;--> statement-breakpoint
+ALTER TABLE "action" ALTER COLUMN "view" SET DEFAULT 'unknown'::text;--> statement-breakpoint
+-- Migrate the view = explorer to the new library view
+UPDATE "action" set view = 'library' WHERE view = 'explorer';--> statement-breakpoint
+DROP TYPE "public"."action_view_enum";--> statement-breakpoint
+CREATE TYPE "public"."action_view_enum" AS ENUM('builder', 'player', 'library', 'account', 'home', 'auth', 'unknown');--> statement-breakpoint
+ALTER TABLE "action" ALTER COLUMN "view" SET DEFAULT 'unknown'::"public"."action_view_enum";--> statement-breakpoint
+ALTER TABLE "action" ALTER COLUMN "view" SET DATA TYPE "public"."action_view_enum" USING "view"::"public"."action_view_enum";
