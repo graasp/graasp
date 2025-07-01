887c409fade4dba3815488950728db0bf0973eff
spaenleh <spaenleh@gmail.com>
2025-07-01 06:45:57 +0000

    fix: tests with isolation of passport instances

--- src/plugins/admin/admin.plugin.ts
+++ src/plugins/admin/admin.plugin.ts
@@ -2,9 +2,9 @@ import { StatusCodes } from 'http-status-codes';
 import { Profile as GitHubProfile, Strategy as GitHubStrategy } from 'passport-github2';
 
 import { createError } from '@fastify/error';
-import fastifyPassport from '@fastify/passport';
+import { Authenticator } from '@fastify/passport';
 import { fastifySecureSession } from '@fastify/secure-session';
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance, FastifyRequest } from 'fastify';
 
 import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '../../config/admin';
 import { PROD } from '../../config/env';
@@ -18,10 +18,10 @@ import { queueDashboardPlugin } from '../../workers/dashboard.controller';
 import { AdminRepository, AdminUser } from './admin.repository';
 
 // module augmentation so the types are right when getting the admin user
-declare module 'fastify' {
-  interface PassportUser {
-    admin?: AdminUser;
-  }
+// this interface can be used in place of the FastifyRequest in request handlers to get correct typing when inside the admin plugin.
+// we use this manual approache to not pollute the global type system with type augmentation.
+interface AdminRequest extends FastifyRequest {
+  admin?: AdminUser;
 }
 
 // name of the passport strategy
@@ -53,10 +53,13 @@ export default async (fastify: FastifyInstance) => {
     },
     expiry: ADMIN_SESSION_EXPIRATION_IN_SECONDS,
   });
-  await fastify.register(fastifyPassport.initialize());
-  await fastify.register(fastifyPassport.secureSession());
+  // declare a new authenticator so that passport for admins does not interfere with the user passport strategies
+  // also set the user property to `admin` so that it will be available on `request.admin`
+  const adminPassport = new Authenticator({ userProperty: 'admin' });
+  await fastify.register(adminPassport.initialize());
+  await fastify.register(adminPassport.secureSession());
 
-  fastifyPassport.use(
+  adminPassport.use(
     GITHUB_OAUTH_STRATEGY,
     new GitHubStrategy(
       {
@@ -64,7 +67,7 @@ export default async (fastify: FastifyInstance) => {
         clientSecret: GITHUB_CLIENT_SECRET,
         callbackURL: `${PUBLIC_URL.origin}/admin/auth/github/callback`,
       },
-      async (accessToken, refreshToken, profile: GitHubProfile, done) => {
+      async (accessToken: string, refreshToken: string, profile: GitHubProfile) => {
         const { username } = profile;
         if (!username) {
           throw new MissingGithubUsername();
@@ -75,20 +78,20 @@ export default async (fastify: FastifyInstance) => {
           // update info stored in the table
           await adminRepository.update(db, username, { id: profile.id });
           // You can add admin checks here
-          return done(null, profile);
+          return profile;
         }
         console.debug('user is not an allowed admin', profile);
-        return done(new NotAnAuthorizedAdmin());
+        throw new NotAnAuthorizedAdmin();
       },
     ),
   );
 
-  fastifyPassport.registerUserSerializer(async (user: GitHubProfile, req) => {
+  adminPassport.registerUserSerializer(async (user: GitHubProfile, req: AdminRequest) => {
     req.log.info(user);
     return user.id;
   });
-  fastifyPassport.registerUserDeserializer(
-    async (uuid: string, req): Promise<{ admin: AdminUser | undefined }> => {
+  adminPassport.registerUserDeserializer(
+    async (uuid: string, req: AdminRequest): Promise<{ admin: AdminUser | undefined }> => {
       req.log.info('uuuid', uuid);
 
       const admin = await adminRepository.get(db, uuid);
@@ -100,32 +103,31 @@ export default async (fastify: FastifyInstance) => {
   fastify.get(
     '/admin/auth/github',
     {
-      preValidation: fastifyPassport.authenticate(GITHUB_OAUTH_STRATEGY, { scope: ['user:email'] }),
+      preValidation: adminPassport.authenticate(GITHUB_OAUTH_STRATEGY, { scope: ['user:email'] }),
     },
-    async (_request, _reply) => {},
+    async (_request: AdminRequest, _reply) => {},
   );
 
   fastify.get(
     '/admin/auth/github/callback',
     {
-      preValidation: fastifyPassport.authenticate(GITHUB_OAUTH_STRATEGY, {
+      preValidation: adminPassport.authenticate(GITHUB_OAUTH_STRATEGY, {
         failureRedirect: '/admin/login',
       }),
     },
-    async (request, reply) => {
-      request.log.info('callback user', request.user);
+    async (_request: AdminRequest, reply) => {
       reply.redirect('/admin');
     },
   );
 
   // login page when user is not authenticated
-  fastify.get('/admin/login', async (_request, reply) => {
+  fastify.get('/admin/login', async (_request: AdminRequest, reply) => {
     reply.type('text/html').send('<a href="/admin/auth/github">Login with GitHub</a>');
   });
 
   // this redirects all unauthenticated requests to the login
   // only /admin/login and /admin/auth/github should be let through to prevent redirection loops
-  fastify.addHook('preHandler', (request, reply, done) => {
+  fastify.addHook('preHandler', (request: AdminRequest, reply, done) => {
     const url = request.raw.url || '';
     if (
       url.startsWith('/admin') &&
@@ -140,17 +142,17 @@ export default async (fastify: FastifyInstance) => {
   });
 
   // return the admin home, for the moment it is a bit bare
-  fastify.get('/admin', async (request, reply) => {
-    request.log.info(request.user);
+  fastify.get('/admin', async (request: AdminRequest, reply) => {
+    request.log.info(request.admin);
     reply.type('text/html').send(
-      `Hello, ${request.user?.admin?.userName || 'admin'}!
+      `Hello, ${request.admin?.userName || 'admin'}!
         <a href="/admin/logout">Logout</a><br/>
         <a href="/admin/queue">Queue Dashboard</a>
         `,
     );
   });
 
-  fastify.get('/admin/logout', async (request, reply) => {
+  fastify.get('/admin/logout', async (request: AdminRequest, reply) => {
     await request.logout();
     reply.redirect('/admin/login');
   });
