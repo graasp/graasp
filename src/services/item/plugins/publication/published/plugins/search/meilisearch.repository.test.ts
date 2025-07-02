763459b5095850f74f9015e6535b0f0a84ffbebf
kim <kim.phanhoang@epfl.ch>
2025-07-02 09:59:39 +0000

    feat: optimize query for indexing items

---
+++ src/services/item/plugins/publication/published/plugins/search/meilisearch.repository.test.ts
@@ -0,0 +1,233 @@
+import { seedFromJson } from '../../../../../../../../test/mocks/seed';
+import { db } from '../../../../../../../drizzle/db';
+import { tagsTable } from '../../../../../../../drizzle/schema';
+import { ItemType } from '../../../../../../../drizzle/types';
+import { assertIsDefined } from '../../../../../../../utils/assertions';
+import { TagCategory } from '../../../../../../tag/tag.schemas';
+import { MeilisearchRepository } from './meilisearch.repository';
+
+const meilisearchRepository = new MeilisearchRepository();
+
+describe('Meilisearch Repository', () => {
+  beforeEach(async () => {
+    // delete tag table to avoid collision
+    await db.delete(tagsTable);
+  });
+
+  describe('getIndexedTree', () => {
+    it('return correct items', async () => {
+      const { items, members, publishedItems } = await seedFromJson({
+        actor: null,
+        items: [
+          {
+            isPublic: true,
+            isPublished: true,
+            creator: { name: 'cedric' },
+            name: 'parent',
+            description: 'parent description',
+            tags: [{ category: TagCategory.Level, name: 'mytag2' }],
+            children: [
+              {
+                name: 'document',
+                type: 'document',
+                description: 'document description',
+                extra: { document: { content: 'document content with some <div>html</div>' } },
+              },
+              {
+                name: 'file',
+                description: null,
+                type: 'file',
+                extra: { file: { content: 'pdf content' } },
+              },
+              { extra: { folder: {} }, type: 'folder', likes: [{ name: 'bob' }, { name: 'anna' }] },
+              {
+                extra: { folder: {} },
+                type: 'folder',
+                tags: [
+                  { category: TagCategory.Discipline, name: 'mytag' },
+                  { category: TagCategory.Discipline, name: 'mytag1' },
+                ],
+              },
+            ],
+          },
+        ],
+      });
+      assertIsDefined(publishedItems);
+
+      const results = await meilisearchRepository.getIndexedTree(db, items[0].path);
+
+      const parent = results.find(({ id }) => id === items[0].id);
+      expect(parent).toEqual({
+        id: items[0].id,
+        name: 'parent',
+        description: 'parent description',
+        type: ItemType.FOLDER,
+        lang: items[0].lang,
+        content: '',
+        level: ['mytag2'],
+        discipline: [],
+        'resource-type': [],
+        creator: { id: members[2].id, name: members[2].name },
+        updatedAt: items[0].updatedAt,
+        createdAt: items[0].createdAt,
+        isPublishedRoot: true,
+        publicationUpdatedAt: publishedItems[0].updatedAt,
+        isHidden: false,
+        likes: 0,
+      });
+
+      const document = results.find(({ id }) => id === items[1].id);
+      expect(document).toEqual({
+        id: items[1].id,
+        name: 'document',
+        description: 'document description',
+        type: 'document',
+        content: 'document content with some html',
+        lang: items[1].lang,
+        level: [],
+        discipline: [],
+        'resource-type': [],
+        creator: { id: '', name: '' },
+        updatedAt: items[1].updatedAt,
+        createdAt: items[1].createdAt,
+        isPublishedRoot: false,
+        publicationUpdatedAt: publishedItems[0].updatedAt,
+        isHidden: false,
+        likes: 0,
+      });
+
+      const file = results.find(({ id }) => id === items[2].id);
+      expect(file).toEqual({
+        id: items[2].id,
+        name: 'file',
+        description: '',
+        type: 'file',
+        content: 'pdf content',
+        lang: items[2].lang,
+        level: [],
+        discipline: [],
+        'resource-type': [],
+        creator: { id: '', name: '' },
+        updatedAt: items[2].updatedAt,
+        createdAt: items[2].createdAt,
+        isPublishedRoot: false,
+        publicationUpdatedAt: publishedItems[0].updatedAt,
+        isHidden: false,
+        likes: 0,
+      });
+
+      const likedItem = results.find(({ id }) => id === items[3].id);
+      expect(likedItem).toEqual({
+        id: items[3].id,
+        name: items[3].name,
+        description: items[3].description,
+        type: items[3].type,
+        content: '',
+        lang: items[3].lang,
+        level: [],
+        discipline: [],
+        'resource-type': [],
+        creator: { id: '', name: '' },
+        updatedAt: items[3].updatedAt,
+        createdAt: items[3].createdAt,
+        isPublishedRoot: false,
+        publicationUpdatedAt: publishedItems[0].updatedAt,
+        isHidden: false,
+        likes: 2,
+      });
+
+      const itemWith2Tags = results.find(({ id }) => id === items[4].id);
+      expect(itemWith2Tags).toEqual({
+        id: items[4].id,
+        name: items[4].name,
+        description: items[4].description,
+        type: items[4].type,
+        content: '',
+        lang: items[4].lang,
+        level: [],
+        discipline: ['mytag', 'mytag1'],
+        'resource-type': [],
+        creator: { id: '', name: '' },
+        updatedAt: items[4].updatedAt,
+        createdAt: items[4].createdAt,
+        isPublishedRoot: false,
+        publicationUpdatedAt: publishedItems[0].updatedAt,
+        isHidden: false,
+        likes: 0,
+      });
+    });
+    it('return nothing for non published item', async () => {
+      const { items } = await seedFromJson({
+        actor: null,
+        items: [
+          {
+            children: [{}],
+          },
+        ],
+      });
+
+      const result = await meilisearchRepository.getIndexedTree(db, items[0].path);
+
+      expect(result).toHaveLength(0);
+    });
+    it('return nothing for deleted item', async () => {
+      const { items } = await seedFromJson({
+        actor: null,
+        items: [
+          {
+            isPublic: true,
+            isPublished: true,
+            isDeleted: true,
+            children: [
+              {
+                isDeleted: true,
+              },
+            ],
+          },
+        ],
+      });
+
+      const result = await meilisearchRepository.getIndexedTree(db, items[0].path);
+
+      expect(result).toHaveLength(0);
+    });
+    it('return nothing for hidden item', async () => {
+      const { items } = await seedFromJson({
+        actor: null,
+        items: [
+          {
+            isPublic: true,
+            isPublished: true,
+            isHidden: true,
+            children: [{}],
+          },
+        ],
+      });
+
+      const result = await meilisearchRepository.getIndexedTree(db, items[0].path);
+
+      expect(result).toHaveLength(0);
+    });
+    it('return only parent if child item is hidden', async () => {
+      const { items } = await seedFromJson({
+        actor: null,
+        items: [
+          {
+            isPublic: true,
+            isPublished: true,
+            children: [
+              {
+                isHidden: true,
+              },
+            ],
+          },
+        ],
+      });
+
+      const result = await meilisearchRepository.getIndexedTree(db, items[0].path);
+
+      expect(result).toHaveLength(1);
+      expect(result[0].id).toEqual(items[0].id);
+    });
+  });
+});
---
+++ src/services/item/plugins/publication/published/plugins/search/meilisearch.repository.ts
@@ -0,0 +1,150 @@
+import { and, count, eq, exists, isNull, sql } from 'drizzle-orm';
+import { singleton } from 'tsyringe';
+
+import { type IndexItem, ItemVisibilityType } from '@graasp/sdk';
+
+import { DBConnection } from '../../../../../../../drizzle/db';
+import { isAncestorOrSelf, isDescendantOrSelf } from '../../../../../../../drizzle/operations';
+import {
+  accountsTable,
+  itemLikesTable,
+  itemTagsTable,
+  itemVisibilitiesTable,
+  items,
+  publishedItemsTable,
+  tagsTable,
+} from '../../../../../../../drizzle/schema';
+import { ItemRaw } from '../../../../../../../drizzle/types';
+import { TagCategory } from '../../../../../../tag/tag.schemas';
+import { stripHtml } from '../../../validation/utils';
+
+@singleton()
+export class MeilisearchRepository {
+  constructor() {}
+
+  public async getIndexedTree(db: DBConnection, itemPath: ItemRaw['path']): Promise<IndexItem[]> {
+    // Define CTE "tree" with descendants and self of given item that are not hidden
+    const tree = db.$with('tree').as(
+      db
+        .select({
+          id: items.id,
+          name: items.name,
+          description: items.description,
+          creatorId: items.creatorId,
+          path: items.path,
+          type: items.type,
+          lang: items.lang,
+          createdAt: items.createdAt,
+          updatedAt: items.updatedAt,
+          extra: items.extra,
+        })
+        .from(items)
+        .leftJoin(
+          itemVisibilitiesTable,
+          and(
+            isAncestorOrSelf(itemVisibilitiesTable.itemPath, items.path),
+            eq(itemVisibilitiesTable.type, ItemVisibilityType.Hidden),
+          ),
+        )
+        .where(and(isDescendantOrSelf(items.path, itemPath), isNull(itemVisibilitiesTable.id))),
+    );
+
+    // Define "tags" CTE with filtered aggregation for each tag category
+    const tagsCte = db.$with('tags').as(
+      db
+        .select({
+          itemId: itemTagsTable.itemId,
+          discipline: sql<
+            string[]
+          >`array_agg(${tagsTable.name}) filter (where ${tagsTable.category}=${TagCategory.Discipline})`.as(
+            'discipline',
+          ),
+          level: sql<
+            string[]
+          >`array_agg(${tagsTable.name}) filter (where ${tagsTable.category}=${TagCategory.Level})`.as(
+            'level',
+          ),
+          resourceType: sql<
+            string[]
+          >`array_agg(${tagsTable.name}) filter (where ${tagsTable.category}=${TagCategory.ResourceType})`.as(
+            'resourceType',
+          ),
+        })
+        .from(tagsTable)
+        .leftJoin(itemTagsTable, eq(tagsTable.id, itemTagsTable.tagId))
+        .where(exists(db.select().from(tree).where(eq(tree.id, itemTagsTable.itemId))))
+        .groupBy(itemTagsTable.itemId),
+    );
+
+    // Define "likes" CTE counting likes per item
+    const likesCte = db.$with('likes').as(
+      db
+        .select({
+          itemId: itemLikesTable.itemId,
+          count: count(itemLikesTable.itemId).as('count'),
+        })
+        .from(itemLikesTable)
+        .where(exists(db.select().from(tree).where(eq(tree.id, itemLikesTable.itemId))))
+        .groupBy(itemLikesTable.itemId),
+    );
+
+    // Build main query referencing the CTEs and joins
+    const result = await db
+      .with(tree, tagsCte, likesCte)
+      .select({
+        // item properties
+        id: tree.id,
+        name: tree.name,
+        description: tree.description,
+        path: tree.path,
+        type: tree.type,
+        // get content for documents and files
+        content: sql<string>`COALESCE(${tree.extra}->'document'->>'content', ${tree.extra}->'file'->>'content','')`,
+        lang: tree.lang,
+        createdAt: tree.createdAt,
+        updatedAt: tree.updatedAt,
+        // publication
+        publishedRootPath: publishedItemsTable.itemPath,
+        publicationUpdatedAt: publishedItemsTable.updatedAt,
+        // tags
+        // use coalesce to return an empty array if the item does not have tags
+        discipline: sql<string[]>`COALESCE(${tagsCte.discipline},'{}')`,
+        level: sql<string[]>`COALESCE(${tagsCte.level},'{}')`,
+        resourceType: sql<string[]>`COALESCE(${tagsCte.resourceType},'{}')`,
+        // creator
+        creatorId: accountsTable.id,
+        creatorName: accountsTable.name,
+        // likes
+        // coalesce in case item does not have any like
+        likes: sql<number>`COALESCE(${likesCte.count},0)::int`,
+      })
+      .from(tree)
+      .innerJoin(publishedItemsTable, isAncestorOrSelf(publishedItemsTable.itemPath, tree.path))
+      .leftJoin(accountsTable, eq(accountsTable.id, tree.creatorId))
+      .leftJoin(tagsCte, eq(tagsCte.itemId, tree.id))
+      .leftJoin(likesCte, eq(likesCte.itemId, tree.id));
+
+    return result.map((doc) => ({
+      id: doc.id,
+      name: doc.name,
+      creator: {
+        id: doc.creatorId ?? '',
+        name: doc.creatorName ?? '',
+      },
+      description: doc.description ? stripHtml(doc.description) : '',
+      type: doc.type,
+      content: doc.content ? stripHtml(doc.content) : '',
+      isPublishedRoot: doc.path === doc.publishedRootPath,
+      publicationUpdatedAt: doc.publicationUpdatedAt,
+      // to be removed
+      isHidden: false,
+      createdAt: doc.createdAt,
+      updatedAt: doc.updatedAt,
+      lang: doc.lang,
+      likes: doc.likes,
+      discipline: doc.discipline,
+      level: doc.level,
+      'resource-type': doc.resourceType,
+    }));
+  }
+}
