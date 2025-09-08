d3a52e14d2e37c7fd8362c2fb37a4276451d31d6
spaenleh <spaenleh@gmail.com>
2025-09-08 11:08:42 +0000

    fix: use arm build

--- docker/build.sh
+++ docker/build.sh
@@ -37,11 +37,11 @@ workers_tag_full="$aws_ecr_uri/$workers_tag_short"
 migrate_tag_short="graasp:migrate-$tag_version"
 migrate_tag_full="$aws_ecr_uri/$migrate_tag_short"
 
-docker build -t $core_tag_full -f docker/Dockerfile --platform linux/amd64 --build-arg APP_VERSION=$tag_version --build-arg BUILD_TIMESTAMP=$(date -u +%Y-%m-%dT%H:%M:%S) .
+docker build -t $core_tag_full -f docker/Dockerfile --build-arg APP_VERSION=$tag_version --build-arg BUILD_TIMESTAMP=$(date -u +%Y-%m-%dT%H:%M:%S) .
 docker push $core_tag_full
 
-docker build -t $workers_tag_full -f docker/workers.Dockerfile --platform linux/amd64 --build-arg APP_VERSION=$tag_version --build-arg BUILD_TIMESTAMP=$(date -u +%Y-%m-%dT%H:%M:%S) .
+docker build -t $workers_tag_full -f docker/workers.Dockerfile --build-arg APP_VERSION=$tag_version --build-arg BUILD_TIMESTAMP=$(date -u +%Y-%m-%dT%H:%M:%S) .
 docker push $workers_tag_full
 
-docker build -t $migrate_tag_full -f docker/migrate.Dockerfile --platform linux/amd64 .
+docker build -t $migrate_tag_full -f docker/migrate.Dockerfile .
 docker push $migrate_tag_full
