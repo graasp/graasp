4cae28b65f837aab8d7b7ac5a8775511cf835a3f
spaenleh <spaenleh@gmail.com>
2025-10-16 14:57:08 +0000

    fix: add migration to insert migration from phoenix for planned migration

---
+++ src/drizzle/0013_add-planned-maintenance-phoenix.sql
@@ -0,0 +1,3 @@
+-- add the migration needed by phoenix to have access to the planned maintenance table
+INSERT INTO "schema_migrations" ("version", "inserted_at") VALUES
+('20251014122320', '2025-10-15 10:28:02');
