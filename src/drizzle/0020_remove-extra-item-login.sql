37ec351643f344c2ce4495e4acc80e6a43ac78c9
Kim Lan Phan Hoang <pyphilia@gmail.com>
2026-02-26 11:52:46 +0100

    refactor: apply suggestion

--- src/drizzle/0020_remove-extra-item-login.sql
+++ src/drizzle/0020_remove-extra-item-login.sql
@@ -4,5 +4,5 @@ update item set extra = extra - 'itemLogin' where extra ? 'itemLogin';
 -- newly empty extra should be replaced for folders
 update item set extra='{"folder":{}}'::jsonb where type='folder' and extra = '{}';
 
--- empty extra from invalid recycle bin because it is not used anymore
+-- delete invalid recycle bin items because they are not used anymore
 delete from item where "type"='recycleBin'
