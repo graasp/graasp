70ccba748b8e70e15f36e331c6893572c5d4e592
Basile Spaenlehauer <spaenleh@gmail.com>
2026-03-03 17:48:50 +0100

    fix: allow multiple spaces in item name (#2091)

--- src/schemas/regex.spec.ts
+++ src/schemas/regex.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 
-import { EMPTY_OR_SPACED_WORDS_REGEX, ITEM_NAME_REGEX } from './regex';
+import { ITEM_NAME_REGEX } from './regex';
 
 describe('Globals', () => {
   describe('Item name regex', () => {
@@ -8,32 +8,12 @@ describe('Globals', () => {
       expect(new RegExp(ITEM_NAME_REGEX).test('Course')).toBeTruthy();
       expect(new RegExp(ITEM_NAME_REGEX).test('My Course')).toBeTruthy();
       expect(new RegExp(ITEM_NAME_REGEX).test('My Course from yesterday')).toBeTruthy();
+      // double space is allowed (users have created content with it already)
+      expect(new RegExp(ITEM_NAME_REGEX).test('My  Course')).toBeTruthy();
     });
     it('Name regex rejects string ending with spaces', () => {
       expect(new RegExp(ITEM_NAME_REGEX).test('My Course from yesterday ')).toBeFalsy();
       expect(new RegExp(ITEM_NAME_REGEX).test(' Course')).toBeFalsy();
-      expect(new RegExp(ITEM_NAME_REGEX).test('My  Course')).toBeFalsy();
-    });
-  });
-
-  describe('Empty or spaced words regex', () => {
-    it('Regex accepts words separated with one space', () => {
-      expect(new RegExp(EMPTY_OR_SPACED_WORDS_REGEX).test('Bob')).toBeTruthy();
-      expect(new RegExp(EMPTY_OR_SPACED_WORDS_REGEX).test('Bob and Alice')).toBeTruthy();
-      expect(new RegExp(EMPTY_OR_SPACED_WORDS_REGEX).test('Bob and Alice')).toBeTruthy();
-    });
-    it('Regex accepts empty string', () => {
-      expect(new RegExp(EMPTY_OR_SPACED_WORDS_REGEX).test('')).toBeTruthy();
-    });
-    it('Regex rejects string ending with spaces', () => {
-      expect(new RegExp(EMPTY_OR_SPACED_WORDS_REGEX).test('Bob ')).toBeFalsy();
-    });
-    it('Regex rejects string starting with spaces', () => {
-      expect(new RegExp(EMPTY_OR_SPACED_WORDS_REGEX).test(' Bob')).toBeFalsy();
-    });
-    it('Regex rejects words separated with multiple spaces', () => {
-      expect(new RegExp(EMPTY_OR_SPACED_WORDS_REGEX).test('Bob  Alice')).toBeFalsy();
-      expect(new RegExp(EMPTY_OR_SPACED_WORDS_REGEX).test('Bob   Alice')).toBeFalsy();
     });
   });
 });
